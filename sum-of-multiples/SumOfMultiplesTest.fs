a6bc073f947fc0b5ec5a6d14dd5dc5ee012c8892
Jon <jwood803@users.noreply.github.com>
2014-05-09 17:33:58 -0400

    Added sum of multiples exercise

---
+++ sum-of-multiples/SumOfMultiplesTest.fs
@@ -0,0 +1,32 @@
+﻿module SumOfMultiplesTest
+
+open NUnit.Framework
+open SumOfMultiples
+
+[<TestFixture>]
+type SumOfMultiplesTest() =
+    let mutable sumOfMultiples = SumOfMultiples()
+    
+    [<Test>]
+    member tc.Sum_to_1() = 
+        Assert.That(sumOfMultiples.To(0), Is.EqualTo(0))
+
+    [<Test>]
+    [<Ignore>]
+    member tc.Sum_to_3() = 
+        Assert.That(sumOfMultiples.To(3), Is.EqualTo(0))
+
+    [<Test>]
+    [<Ignore>]
+    member tc.Sum_to_10() = 
+        Assert.That(sumOfMultiples.To(10), Is.EqualTo(23))
+
+    [<Test>]
+    [<Ignore>]
+    member tc.Configurable_7_13_17_to_20() = 
+        Assert.That(SumOfMultiples([7; 13; 17]).To(20), Is.EqualTo(51))
+
+    [<Test>]
+    [<Ignore>]
+    member tc.Configurable_43_47_to_10000() = 
+        Assert.That(SumOfMultiples([43; 47]).To(10000), Is.EqualTo(2203160))
