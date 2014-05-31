f4308172b7570d628dcb6de171c189f6dfa9543f
Jon <jwood803@users.noreply.github.com>
2014-05-31 13:20:58 -0400

    Adding the robot name exercise

---
+++ robot-name/RobotNameTest.fs
@@ -0,0 +1,33 @@
+﻿module RobotNameTest
+
+open NUnit.Framework
+open RobotName
+
+[<TestFixture>]
+type RobotNameTest() =
+    let robot = RobotName()
+
+    [<Test>]
+    member tests.Robot_has_a_name() =
+        StringAssert.IsMatch(@"\w{2}\d{3}", robot.Name)
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Name_is_the_same_each_time() =
+        Assert.True(true)
+
+    [<Test>]
+    [<Ignore>]
+    member this.Different_robots_have_different_names() =
+        let robot2 = RobotName()
+
+        Assert.That(robot.Name, Is.Not.EqualTo(robot2.Name))
+
+    [<Test>]
+    [<Ignore>]
+    member this.Can_reset_the_name() =
+        let originalName = robot.Name
+        
+        robot.Reset()
+
+        Assert.That(originalName, Is.Not.EqualTo(robot.Name))
