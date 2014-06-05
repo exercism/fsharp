5cb2d53746fb280caec52715f64e5b0bf8fa5169
Jon <jwood803@users.noreply.github.com>
2014-06-05 16:46:53 -0400

    Add bank account exercise

---
+++ bank-account/Example.fs
@@ -0,0 +1,20 @@
+﻿module BankAccount
+
+type BankAccount() =
+    member val Balance = Option<float>.None with get, set
+
+    member this.openAccount() =
+        this.Balance <- Some 0.0
+        this.Balance.Value
+
+    member this.closeAccount() =
+        this.Balance <- None
+
+    member this.getBalance() =
+        this.Balance.Value
+
+    member this.updateBalance(newBalance: float) =
+        let balance = this.Balance.Value
+        let updatedBalance = balance + newBalance
+
+        this.Balance <- Some updatedBalance
