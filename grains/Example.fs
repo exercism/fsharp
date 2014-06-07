10658e97715390f0472ce4ed3531a92a1bc77223
Jon <jwood803@users.noreply.github.com>
2014-06-06 20:36:05 -0400

    Added grains exercise

---
+++ grains/Example.fs
@@ -0,0 +1,8 @@
+﻿module Grains
+
+type Grains() =
+    member this.square(number: int) =
+        2I ** (number - 1)
+
+    member this.total() =
+        [1I..64I] |> List.reduce(fun accumulator element -> accumulator + this.square(int element))
