e0a0ef8a066a7b885b306aa955d57795e9ff9900
Jon <jwood803@users.noreply.github.com>
2014-10-14 06:28:16 -0500

    Add leap year exercise

---
+++ leap/Example.fs
@@ -0,0 +1,12 @@
+﻿module LeapYear
+
+[<AbstractClass; Sealed>]
+type LeapYear() =
+    static member isLeap year =
+        match year with
+        | y when y % 100 = 0 -> 
+            match y % 400 = 0 with
+            | true -> true
+            | _ -> false
+        | y when y % 4 = 0 -> true
+        | _ -> false
