4ce09c4e03da32c8403dc1b49be8540151041a0c
Erik Schierboom <erik_schierboom@hotmail.com>
2015-02-11 12:23:57 +0100

    Added word-count exercise

---
+++ word-count/Example.fs
@@ -0,0 +1,12 @@
+﻿module Phrase
+
+open System.Text.RegularExpressions
+
+type Phrase(phrase: string) =
+
+    member this.wordCount() = 
+        Regex.Matches(phrase.ToLowerInvariant(), @"\w+('\w+)*") 
+        |> Seq.cast<Match> 
+        |> Seq.groupBy (fun m -> m.Value)
+        |> Seq.map (fun (word, occurrences) -> word, occurrences |> Seq.length)                     
+        |> Map.ofSeq
