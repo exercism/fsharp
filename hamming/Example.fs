17dd69185412135e7db8ae1ed98356378fab8bc5
Erik Schierboom <erik_schierboom@hotmail.com>
2015-02-11 09:31:54 +0100

    Added Hamming exercise

---
+++ hamming/Example.fs
@@ -0,0 +1,7 @@
+﻿module Hamming
+
+type Hamming() =
+    member this.compute(strand1:string, strand2:string) = 
+        Array.zip (strand1.ToCharArray()) (strand2.ToCharArray()) |> 
+        Array.filter (fun (c1, c2) -> c1 <> c2) |>
+        Array.length
