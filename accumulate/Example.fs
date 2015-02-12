3473d90e84a14ca4c78dd7dacd59d71d82c0c0d0
Erik Schierboom <erik_schierboom@hotmail.com>
2015-02-12 09:14:11 +0100

    Added accumulate exercise

---
+++ accumulate/Example.fs
@@ -0,0 +1,11 @@
+﻿module Accumulate
+
+type Accumulate() =
+
+    let rec accumulateLoop func input acc = 
+        match input with
+            | [] -> acc |> List.rev
+            | x::xs -> accumulateLoop func xs (func x :: acc)
+
+    member this.accumulate(func, input) = accumulateLoop func input List.empty
+
