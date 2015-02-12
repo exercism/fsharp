31311da9317a59f4870ed31245080548498b2997
Erik Schierboom <erik_schierboom@hotmail.com>
2015-02-12 11:42:38 +0100

    Added allergies exercise

---
+++ allergies/Example.fs
@@ -0,0 +1,22 @@
+﻿module Allergies
+
+open System
+
+type Allergen = 
+   | Eggs = 1
+   | Peanuts = 2
+   | Shellfish = 4
+   | Strawberries = 8
+   | Tomatoes = 16
+   | Chocolate = 32
+   | Pollen = 64
+   | Cats = 128
+
+type Allergies(codedAllergies) =
+
+    member this.allergicTo(allergen: Allergen) = codedAllergies &&& int allergen <> 0
+
+    member this.list() = Enum.GetValues(typeof<Allergen>) 
+                         |> Seq.cast<Allergen> 
+                         |> List.ofSeq 
+                         |> List.filter this.allergicTo
