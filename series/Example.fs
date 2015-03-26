ce3750d0208262a334dfb8eaa135872cd1ca89b1
Erik Schierboom <erik_schierboom@hotmail.com>
2015-03-26 09:44:58 +0100

    Added Series exercise

---
+++ series/Example.fs
@@ -0,0 +1,17 @@
+module Series
+
+open System
+
+type Series(str:string) = 
+
+    let charToDigit c = c.ToString() |> Int32.Parse 
+    let digits = str |> List.ofSeq |> List.map charToDigit
+    let slice length input = Seq.take length input |> List.ofSeq
+
+    let rec slicesHelper length remainder acc = 
+        if remainder |> List.length < length then acc |> List.rev
+        else slicesHelper length (remainder |> List.tail) (slice length remainder:: acc)
+
+    member this.slices(length) = 
+        if length > str.Length then invalidArg "length" "Slice length must not be than input length"
+        else slicesHelper length digits []
---
+++ series/SeriesTests.fs
@@ -0,0 +1,80 @@
+module SeriesTests
+
+open System
+open NUnit.Framework
+open Series
+
+[<TestFixture>]
+type SeriesTests() =
+    static member SliceOneTestData = 
+        [| 
+            ("01234", [[0]; [1]; [2]; [3]; [4]]);
+            ("92834", [[9]; [2]; [8]; [3]; [4]]) 
+        |]
+
+    [<TestCaseSource("SliceOneTestData")>]
+    member tests.Series_of_one_splits_to_one_digit(testData: string * int list list) =
+        let input, expected = testData
+        let series = new Series(input)
+        Assert.That(series.slices(1), Is.EqualTo(expected))
+
+    static member SliceTwoTestData =
+        [| 
+            ("01234", [[0; 1]; [1; 2]; [2; 3]; [3; 4]]);
+            ("98273463", [[9; 8]; [8; 2]; [2; 7]; [7; 3]; [3; 4]; [4; 6]; [6; 3]]);
+            ("37103", [[3; 7]; [7; 1]; [1; 0]; [0; 3]])
+        |]
+
+    [<TestCaseSource("SliceTwoTestData")>]
+    [<Ignore>]
+    member tests.Series_of_two_splits_to_two_digits(testData: string * int list list) =
+        let input, expected = testData
+        let series = new Series(input)
+        Assert.That(series.slices(2), Is.EqualTo(expected))
+
+    static member SliceThreeTestData =
+        [| 
+            ("01234", [[0; 1; 2]; [1; 2; 3]; [2; 3; 4]]);
+            ("31001", [[3; 1; 0]; [1; 0; 0]; [0; 0; 1]]);
+            ("982347", [[9; 8; 2]; [8; 2; 3]; [2; 3; 4]; [3; 4; 7]])
+        |]
+
+    [<TestCaseSource("SliceThreeTestData")>]
+    [<Ignore>]
+    member tests.Series_of_three_splits_to_three_digits(testData: string * int list list) =
+        let input, expected = testData
+        let series = new Series(input)
+        Assert.That(series.slices(3), Is.EqualTo(expected))
+
+    static member SliceFourTestData =
+        [| 
+            ("01234", [[0; 1; 2; 3]; [1; 2; 3; 4]]);
+            ("91274", [[9; 1; 2; 7]; [1; 2; 7; 4]])
+        |]
+
+    [<TestCaseSource("SliceFourTestData")>]
+    [<Ignore>]
+    member tests.Series_of_four_splits_to_four_digits(testData: string * int list list) =
+        let input, expected = testData
+        let series = new Series(input)
+        Assert.That(series.slices(4), Is.EqualTo(expected))
+
+    static member SliceFiveTestData =
+        [| 
+            ("01234", [[0; 1; 2; 3; 4]]);
+            ("81228", [[8; 1; 2; 2; 8]])
+        |]
+
+    [<TestCaseSource("SliceFiveTestData")>]
+    [<Ignore>]
+    member tests.Series_of_five_splits_to_five_digits(testData: string * int list list) =
+        let input, expected = testData
+        let series = new Series(input)
+        Assert.That(series.slices(5), Is.EqualTo(expected))
+
+    [<TestCase("01234", 6)>]
+    [<TestCase("01032987583", 19)>]
+    [<Ignore>]
+    member tests.Slice_longer_than_input_is_not_allowed(input, slice) =
+        let series = new Series(input)
+        Assert.Throws<System.ArgumentException>(fun () -> series.slices(slice) |> ignore) |> ignore
