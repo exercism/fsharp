03db6d252e89022bbab706d7a51ca2b04b9168c1
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-01 12:52:27 +0200

    Added crypto-square exercise

---
+++ crypto-square/CryptoSquareTests.fs
@@ -0,0 +1,84 @@
+module CryptoSquareTests
+
+open NUnit.Framework
+open CryptoSquare
+
+[<TestFixture>]
+type CryptoSquareTests() =
+
+    [<Test>]
+    member tests.Strange_characters_are_stripped_during_normalization() =
+        let crypto = new CryptoSquare("s#$%^&plunk")
+        Assert.That(crypto.normalizePlaintext, Is.EqualTo("splunk"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Letters_are_lowercased_during_normalization() =
+        let crypto = new CryptoSquare("WHOA HEY!")
+        Assert.That(crypto.normalizePlaintext, Is.EqualTo("whoahey"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Numbers_are_kept_during_normalization() =
+        let crypto = new CryptoSquare("1, 2, 3, GO!")
+        Assert.That(crypto.normalizePlaintext, Is.EqualTo("123go"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Smallest_square_size_is_2() =
+        let crypto = new CryptoSquare("1234")
+        Assert.That(crypto.size, Is.EqualTo(2))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Size_of_text_whose_length_is_a_perfect_square_is_its_square_root() =
+        let crypto = new CryptoSquare("123456789")
+        Assert.That(crypto.size, Is.EqualTo(3))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Size_of_text_whose_length_is_not_a_perfect_square_is_next_biggest_square_root() =
+        let crypto = new CryptoSquare("123456789abc")
+        Assert.That(crypto.size, Is.EqualTo(4))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Size_is_determined_by_normalized_text() =
+        let crypto = new CryptoSquare("Oh hey, this is nuts!")
+        Assert.That(crypto.size, Is.EqualTo(4))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Segments_are_split_by_square_size() =
+        let crypto = new CryptoSquare("Never vex thine heart with idle woes")
+        Assert.That(crypto.plaintextSegments(), Is.EqualTo(["neverv"; "exthin"; "eheart"; "withid"; "lewoes"]))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Segments_are_split_by_square_size_until_text_runs_out() =
+        let crypto = new CryptoSquare("ZOMG! ZOMBIES!!!")
+        Assert.That(crypto.plaintextSegments(), Is.EqualTo(["zomg"; "zomb"; "ies"]))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Ciphertext_combines_text_by_column() =
+        let crypto = new CryptoSquare("First, solve the problem. Then, write the code.")
+        Assert.That(crypto.ciphertext(), Is.EqualTo("foeewhilpmrervrticseohtottbeedshlnte"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Ciphertext_skips_cells_with_no_text() =
+        let crypto = new CryptoSquare("Time is an illusion. Lunchtime doubly so.")
+        Assert.That(crypto.ciphertext(), Is.EqualTo("tasneyinicdsmiohooelntuillibsuuml"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Normalized_ciphertext_is_split_by_5() =
+        let crypto = new CryptoSquare("Vampires are people too!")
+        Assert.That(crypto.normalizeCiphertext(), Is.EqualTo("vrel aepe mset paoo irpo"))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Normalized_ciphertext_not_exactly_divisible_by_5_spills_into_a_smaller_segment() =
+        let crypto = new CryptoSquare("Madness, and then illumination.")
+        Assert.That(crypto.normalizeCiphertext(), Is.EqualTo("msemo aanin dnin ndla etlt shui"))
---
+++ crypto-square/Example.fs
@@ -0,0 +1,25 @@
+﻿module CryptoSquare
+
+open System
+
+type CryptoSquare(input) = 
+
+    let charsToString chars = new String(chars |> Array.ofSeq)
+
+    let chunksOfSize n seq = 
+        seq 
+        |> Seq.mapi(fun i x -> i / n, x)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (_, g) -> g |> Seq.map snd |> charsToString)
+
+    let transpose seq = 
+        seq
+        |> Seq.collect(fun s -> s |> Seq.mapi(fun i e -> (i, e)))
+        |> Seq.groupBy(fst)
+        |> Seq.map(fun (i, s) -> s |> Seq.map snd |> charsToString)    
+
+    member this.size() = this.normalizePlaintext() |> String.length |> float |> Math.Sqrt |> ceil |> int
+    member this.normalizePlaintext() = seq { for c in input do if Char.IsLetterOrDigit c then yield Char.ToLowerInvariant c } |> charsToString
+    member this.plaintextSegments() = chunksOfSize (this.size()) (this.normalizePlaintext()) |> List.ofSeq
+    member this.ciphertext() = this.plaintextSegments() |> transpose |> String.concat ""
+    member this.normalizeCiphertext() = this.plaintextSegments() |> transpose |> String.concat " "
