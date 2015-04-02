28a5c6e106ad84d39de7e2e5e837a255afce1c2d
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-02 16:17:42 +0200

    Added Luhn exercise

---
+++ luhn/Example.fs
@@ -0,0 +1,23 @@
+﻿module Luhn
+
+type Luhn(number:int64) =
+        
+    let digit c = (int)c - (int)'0'
+    let digits = number.ToString() |> Seq.map digit
+
+    let addend index digit = 
+        if (index % 2 = 0) then digit else 
+        if (digit >= 5) then digit * 2 - 9 
+        else digit * 2
+
+    member this.checkDigit = number % 10L
+    member this.addends = digits |> List.ofSeq |> List.rev |> List.mapi addend |> List.rev
+    member this.checksum = (this.addends |> List.sum) % 10
+    member this.valid = this.checksum = 0
+
+    static member create(target:int64) = 
+        let targetNumber = target * 10L
+        let targetLuhn = Luhn(targetNumber)
+
+        if targetLuhn.valid then targetNumber
+        else targetNumber + 10L - (int64)targetLuhn.checksum
---
+++ luhn/LuhnTests.fs
@@ -0,0 +1,45 @@
+module LuhnTests
+
+open NUnit.Framework
+open Luhn
+
+[<TestFixture>]
+type LuhnTests() =    
+    [<Test>]
+    member tests.Check_digit_is_the_rightmost_digit() =
+        Assert.That(Luhn(34567L).checkDigit, Is.EqualTo(7))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Addends_doubles_every_other_number_from_the_right() =
+        Assert.That(Luhn(12121L).addends, Is.EqualTo([1; 4; 1; 4; 1]))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Addends_subtracts_9_when_doubled_number_is_more_than_9() =
+        Assert.That(Luhn(8631L).addends, Is.EqualTo([7; 6; 6; 1]))
+
+    [<TestCase(4913, Result = 2, Ignore = true)>]
+    [<TestCase(201773, Result = 1, Ignore = true)>]
+    member tests.Checksum_adds_addends_together(number) =
+        Luhn(number).checksum
+
+    [<TestCase(738, Result = false, Ignore = true)>]
+    [<TestCase(8739567, Result = true, Ignore = true)>]
+    member tests.Number_is_valid_when_checksum_mod_10_is_zero(number) =
+        Luhn(number).valid
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Luhn_can_create_simple_numbers_with_valid_check_digit() =
+        Assert.That(Luhn.create(123L), Is.EqualTo(1230))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Luhn_can_create_larger_numbers_with_valid_check_digit() =
+        Assert.That(Luhn.create(873956L), Is.EqualTo(8739567))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Luhn_can_create_huge_numbers_with_valid_check_digit() =
+        Assert.That(Luhn.create(837263756L), Is.EqualTo(8372637564L))
