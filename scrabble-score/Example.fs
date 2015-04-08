22b6946f01393b0356efdf65db021defd8c18100
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-08 14:07:17 +0200

    Added scrabble-score exercise

---
+++ scrabble-score/Example.fs
@@ -0,0 +1,16 @@
+﻿module Scrabble
+
+open System
+
+type Scrabble(word:string) =
+
+    let letterScores = [("AEIOULNRST", 1); ("DG", 2); ("BCMP", 3); ("FHVWY", 4); ("K", 5); ("JX", 9); ("QZ", 10)] 
+                       |> List.map (fun (letters, score) -> letters |> Seq.map (fun letter -> (letter, score))) 
+                       |> Seq.concat 
+                       |> Map.ofSeq
+
+    let scoreLetter letter = defaultArg (Map.tryFind letter letterScores) 0
+
+    member val score = word.ToUpperInvariant() |> Seq.sumBy scoreLetter
+
+    static member wordScore(word) = Scrabble(word).score
---
+++ scrabble-score/ScrabbleScoreTests.fs
@@ -0,0 +1,46 @@
+module ScrabbleScoreTests
+
+open NUnit.Framework
+open Scrabble
+
+[<TestFixture>]
+type ScrabbleScoreTest() = 
+  
+    [<Test>]
+    member tests.Empty_word_scores_zero() =
+        Assert.That(Scrabble("").score, Is.EqualTo(0))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Whitespace_scores_zero() =
+        Assert.That(Scrabble(" \t\n").score, Is.EqualTo(0))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.scores_very_short_word() =
+        Assert.That(Scrabble("a").score, Is.EqualTo(1))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.scores_other_very_short_word() =
+        Assert.That(Scrabble("f").score, Is.EqualTo(4))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Simple_word_scores_the_number_of_letters() =
+        Assert.That(Scrabble("street").score, Is.EqualTo(6))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Complicated_word_scores_more() =
+        Assert.That(Scrabble("quirky").score, Is.EqualTo(22))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.scores_are_case_insensitive() =
+        Assert.That(Scrabble("MULTIBILLIONAIRE").score, Is.EqualTo(20))
+
+    [<Test>]
+    [<Ignore>]
+    member tests.Convenient_scoring() =
+        Assert.That(Scrabble.wordScore("alacrity"), Is.EqualTo(13))
