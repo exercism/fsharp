e00d27f58f5f87dc7e128efd921efcdf34c5135e
Mathew Glodack <glodack@gmail.com>
2016-01-02 09:37:26 -0800

    Remove immutable value from test file

--- sum-of-multiples/SumOfMultiplesTest.fs
+++ sum-of-multiples/SumOfMultiplesTest.fs
@@ -5,38 +5,36 @@ open SumOfMultiples
 
 [<TestFixture>]
 type SumOfMultiplesTest() =
-    let mutable sumOfMultiples = SumOfMultiples()
-    
     [<Test>]
-    member tc.Sum_to_1() = 
-        Assert.That(sumOfMultiples.To(0), Is.EqualTo(0))
+    member tc.Sum_to_1() =
+        Assert.That(SumOfMultiples().To(0), Is.EqualTo(0))
 
     [<Test>]
     [<Ignore>]
-    member tc.Sum_to_3() = 
-        Assert.That(sumOfMultiples.To(3), Is.EqualTo(0))
+    member tc.Sum_to_3() =
+        Assert.That(SumOfMultiples().To(3), Is.EqualTo(0))
 
     [<Test>]
     [<Ignore>]
-    member tc.Sum_to_10() = 
-        Assert.That(sumOfMultiples.To(10), Is.EqualTo(23))
+    member tc.Sum_to_10() =
+        Assert.That(SumOfMultiples().To(10), Is.EqualTo(23))
 
     [<Test>]
     [<Ignore>]
-    member tc.Sum_to_100() = 
-        Assert.That(sumOfMultiples.To(100), Is.EqualTo(2318))
+    member tc.Sum_to_100() =
+        Assert.That(SumOfMultiples().To(100), Is.EqualTo(2318))
 
     [<Test>]
     [<Ignore>]
-    member tc.Sum_to_1000() = 
-        Assert.That(sumOfMultiples.To(1000), Is.EqualTo(233168))
+    member tc.Sum_to_1000() =
+        Assert.That(SumOfMultiples().To(1000), Is.EqualTo(233168))
 
     [<Test>]
     [<Ignore>]
-    member tc.Configurable_7_13_17_to_20() = 
+    member tc.Configurable_7_13_17_to_20() =
         Assert.That(SumOfMultiples([7; 13; 17]).To(20), Is.EqualTo(51))
 
     [<Test>]
     [<Ignore>]
-    member tc.Configurable_43_47_to_10000() = 
+    member tc.Configurable_43_47_to_10000() =
         Assert.That(SumOfMultiples([43; 47]).To(10000), Is.EqualTo(2203160))
