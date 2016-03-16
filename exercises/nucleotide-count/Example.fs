47d8f0adba23dd753cb7ddc54c80778736ac4e12
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-16 13:02:56 +0100

    Add nucleotide-count exercise

---
+++ exercises/nucleotide-count/Example.fs
@@ -0,0 +1,14 @@
+﻿module NucleoTideCount
+
+let private validNucleotides = ['A'; 'T'; 'C'; 'G']
+
+let private isValid nucleotide = List.contains nucleotide validNucleotides
+
+let count (nucleotide:char) (strand:string) = 
+    match isValid nucleotide with
+    | true  -> Seq.fold (fun acc elem -> if elem = nucleotide then acc + 1 else acc) 0 strand
+    | false -> failwith "Invalid nucleotide"
+
+let nucleotideCounts strand = 
+    List.map (fun nucleotide -> (nucleotide, count nucleotide strand)) validNucleotides 
+    |> Map.ofSeq
