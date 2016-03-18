4c32dc0f1468e2c4696425c5ffd858a5f7e36376
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-18 10:28:17 +0100

    Add sieve exercise

---
+++ exercises/sieve/Example.fs
@@ -0,0 +1,10 @@
+﻿module Sieve
+
+let rec private sieve remainder primes =     
+    match remainder with
+    | [] -> primes |> List.rev
+    | p::xs -> sieve (xs |> List.filter (fun x -> x % p > 0)) (p :: primes)
+    
+let primesUpTo limit = 
+    let possiblePrimes = [2 .. limit]    
+    sieve possiblePrimes []
