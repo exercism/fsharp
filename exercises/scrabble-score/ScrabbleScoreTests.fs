38e1d1aa0f75370c62e28ae821d2051910325cac
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:26:41 +0200

    Convert scrabble-score exercise to idiomatic F#

--- exercises/scrabble-score/ScrabbleScoreTests.fs
+++ exercises/scrabble-score/ScrabbleScoreTests.fs
@@ -2,44 +2,37 @@ module ScrabbleScoreTests
 
 open NUnit.Framework
 open Scrabble
-
-type ScrabbleScoreTest() = 
   
-    [<Test>]
-    member tests.Empty_word_scores_zero() =
-        Assert.That(Scrabble("").score, Is.EqualTo(0))
+[<Test>]
+let ``Empty word scores zero`` () =
+    Assert.That(score "", Is.EqualTo(0))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Whitespace_scores_zero() =
-        Assert.That(Scrabble(" \t\n").score, Is.EqualTo(0))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Whitespace scores zero`` () =
+    Assert.That(score " \t\n", Is.EqualTo(0))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.scores_very_short_word() =
-        Assert.That(Scrabble("a").score, Is.EqualTo(1))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Scores very short word`` () =
+    Assert.That(score "a", Is.EqualTo(1))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.scores_other_very_short_word() =
-        Assert.That(Scrabble("f").score, Is.EqualTo(4))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Scores other very short word`` () =
+    Assert.That(score "f", Is.EqualTo(4))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Simple_word_scores_the_number_of_letters() =
-        Assert.That(Scrabble("street").score, Is.EqualTo(6))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Simple word scores the number of letters`` () =
+    Assert.That(score "street", Is.EqualTo(6))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Complicated_word_scores_more() =
-        Assert.That(Scrabble("quirky").score, Is.EqualTo(22))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Complicated word scores more`` () =
+    Assert.That(score "quirky", Is.EqualTo(22))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.scores_are_case_insensitive() =
-        Assert.That(Scrabble("MULTIBILLIONAIRE").score, Is.EqualTo(20))
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Convenient_scoring() =
-        Assert.That(Scrabble.wordScore("alacrity"), Is.EqualTo(13))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Scores are case insensitive`` () =
+    Assert.That(score "MULTIBILLIONAIRE", Is.EqualTo(20))
