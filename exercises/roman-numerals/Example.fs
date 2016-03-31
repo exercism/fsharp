848b8c829c9f6f46c7fbe5c73dfbcf949b29f518
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:26:26 +0200

    Convert roman-numerals exercise to idiomatic F#

--- exercises/roman-numerals/Example.fs
+++ exercises/roman-numerals/Example.fs
@@ -1,26 +1,24 @@
 ﻿module RomanNumeral
 
-type RomanNumeral() =
+let numeralThresholds = [(1000, "M");
+                            (900,  "CM");
+                            (500,  "D");
+                            (400,  "CD");
+                            (100,  "C");
+                            (90,   "XC");
+                            (50,   "L");
+                            (40,   "XL");
+                            (10,   "X");
+                            (9,    "IX");
+                            (5,    "V");
+                            (4,    "IV");
+                            (1,    "I")]
 
-    let numeralThresholds = [(1000, "M");
-                             (900,  "CM");
-                             (500,  "D");
-                             (400,  "CD");
-                             (100,  "C");
-                             (90,   "XC");
-                             (50,   "L");
-                             (40,   "XL");
-                             (10,   "X");
-                             (9,    "IX");
-                             (5,    "V");
-                             (4,    "IV");
-                             (1,    "I")]
+let rec toRomanLoop remainder acc thresholds =         
+    match thresholds with
+        | [] -> acc
+        | (threshold, numeral)::xs ->
+            if threshold <= remainder then toRomanLoop (remainder - threshold) (acc + numeral) thresholds
+            else toRomanLoop remainder acc xs
 
-    let rec toRomanLoop remainder acc thresholds =         
-        match thresholds with
-            | [] -> acc
-            | (threshold, numeral)::xs ->
-                if threshold <= remainder then toRomanLoop (remainder - threshold) (acc + numeral) thresholds
-                else toRomanLoop remainder acc xs
-
-    member this.toRoman(arabicNumeral: int) = toRomanLoop arabicNumeral "" numeralThresholds
+let toRoman (arabicNumeral: int) = toRomanLoop arabicNumeral "" numeralThresholds
