03ade65de9e8feb5200f2c91b8dd9f4a03d76d1a
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:21:34 +0200

    Convert difference-of-squares exercise to idiomatic F#

--- exercises/difference-of-squares/DifferenceOfSquaresTest.fs
+++ exercises/difference-of-squares/DifferenceOfSquaresTest.fs
@@ -2,49 +2,47 @@
 
 open NUnit.Framework
 open DifferenceOfSquares
-
-type DifferenceOfSquaresTests() =
     
-    [<Test>]
-    member test.Square_of_sums_to_5() =
-        Assert.That(DifferenceOfSquares(5).squareOfSums(), Is.EqualTo(225))
+[<Test>]
+let ``Square of sums to 5`` () =
+    Assert.That(squareOfSums 5, Is.EqualTo(225))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Sum_of_squares_to_5() =
-        Assert.That(DifferenceOfSquares(5).sumOfSquares(), Is.EqualTo(55))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Sum of squares to 5`` () =
+    Assert.That(sumOfSquares 5, Is.EqualTo(55))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Difference_of_sums_to_5() =
-        Assert.That(DifferenceOfSquares(5).difference(), Is.EqualTo(170))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Difference of sums to 5`` () =
+    Assert.That(difference 5, Is.EqualTo(170))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Square_of_sums_to_10() =
-        Assert.That(DifferenceOfSquares(10).squareOfSums(), Is.EqualTo(3025))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Square of sums to 10`` () =
+    Assert.That(squareOfSums 10, Is.EqualTo(3025))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Sum_of_squares_to_10() =
-        Assert.That(DifferenceOfSquares(10).sumOfSquares(), Is.EqualTo(385))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Sum of squares to 10`` () =
+    Assert.That(sumOfSquares 10, Is.EqualTo(385))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Difference_of_sums_to_10() =
-        Assert.That(DifferenceOfSquares(10).difference(), Is.EqualTo(2640))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Difference of sums to 10`` () =
+    Assert.That(difference 10, Is.EqualTo(2640))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Square_of_sums_to_100() =
-        Assert.That(DifferenceOfSquares(100).squareOfSums(), Is.EqualTo(25502500))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Square of sums to 100`` () =
+    Assert.That(squareOfSums 100, Is.EqualTo(25502500))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Sum_of_squares_to_100() =
-        Assert.That(DifferenceOfSquares(100).sumOfSquares(), Is.EqualTo(338350))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Sum of squares to 100`` () =
+    Assert.That(sumOfSquares 100, Is.EqualTo(338350))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member test.Difference_of_sums_to_100() =
-        Assert.That(DifferenceOfSquares(100).difference(), Is.EqualTo(25164150))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Difference of sums to 100`` () =
+    Assert.That(difference 100, Is.EqualTo(25164150))
