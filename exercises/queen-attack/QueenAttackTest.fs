241c10f533ee4b54bf4930444a173301d3c8e204
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:24:51 +0200

    Convert queen-attack exercise to idiomatic F#

--- exercises/queen-attack/QueenAttackTest.fs
+++ exercises/queen-attack/QueenAttackTest.fs
@@ -6,42 +6,42 @@ open NUnit.Framework
 open QueenAttack
 
 [<Test>]
-let ``Cannot occupy same space``() =
+let ``Cannot occupy same space`` () =
     let white = (2, 4)
     let black = (2, 4)
-    Assert.Throws(fun () -> canAttack white black |> ignore) |> ignore
+    Assert.That((fun () -> canAttack white black |> ignore), Throws.Exception)
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Cannot attack``() =
+let ``Cannot attack`` () =
     Assert.False(canAttack (2, 3) (4, 7))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on same row``() =
+let ``Can attack on same row`` () =
     Assert.True(canAttack (2, 4) (2, 7))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on same column``() =
+let ``Can attack on same column`` () =
     Assert.True(canAttack (5, 4) (2, 4))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on diagonal``() =
+let ``Can attack on diagonal`` () =
     Assert.True(canAttack (1, 1) (6, 6))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on other diagonal``() =
+let ``Can attack on other diagonal`` () =
     Assert.True(canAttack (0, 6) (1, 7))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on yet another diagonal``() =
+let ``Can attack on yet another diagonal`` () =
     Assert.True(canAttack (4, 1) (6, 3))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Can attack on a diagonal slanted the other way``() =
+let ``Can attack on a diagonal slanted the other way`` () =
     Assert.True(canAttack (6, 1) (1, 6))
