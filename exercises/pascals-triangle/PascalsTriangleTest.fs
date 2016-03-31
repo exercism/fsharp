b70ee9b02a7ad7c47364cfa913837c2c9e5d7825
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:24:24 +0200

    Convert pascals-triangle exercise to idiomatic F#

--- exercises/pascals-triangle/PascalsTriangleTest.fs
+++ exercises/pascals-triangle/PascalsTriangleTest.fs
@@ -5,30 +5,30 @@ open NUnit.Framework
 open PascalsTriangle
 
 [<Test>]
-let ``One row``() =
+let ``One row`` () =
     Assert.That(triangle 1, Is.EqualTo([[1]]))
         
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Two rows``() =
+let ``Two rows`` () =
     Assert.That(triangle 2, Is.EqualTo([[1]; [1; 1]]))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Three rows``() =
+let ``Three rows`` () =
     Assert.That(triangle 3, Is.EqualTo([[1]; [1; 1]; [1; 2; 1]]))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Four rows``() =
+let ``Four rows`` () =
     Assert.That(triangle 4, Is.EqualTo([[1]; [1; 1]; [1; 2; 1]; [1; 3; 3; 1]]))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Five rows``() =
+let ``Five rows`` () =
     Assert.That(triangle 5, Is.EqualTo([[1]; [1; 1]; [1; 2; 1]; [1; 3; 3; 1]; [1; 4; 6; 4; 1]]))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Twenty rows``() =
+let ``Twenty rows`` () =
     Assert.That(triangle 20 |> List.last, Is.EqualTo([1; 19; 171; 969; 3876; 11628; 27132; 50388; 75582; 92378; 92378; 75582; 50388; 27132; 11628; 3876; 969; 171; 19; 1]))
