266360a2dfef34f174d29717c9db15aff3dd1ec9
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:20:12 +0200

    Convert binary exercise to idiomatic F#

--- exercises/binary/BinaryTests.fs
+++ exercises/binary/BinaryTests.fs
@@ -2,24 +2,22 @@
 
 open NUnit.Framework
 open Binary
-
-type BinaryTests() =
     
-    [<TestCase("1", ExpectedResult = 1)>]
-    [<TestCase("10", ExpectedResult = 2, Ignore = "Remove to run test case")>]
-    [<TestCase("11", ExpectedResult = 3, Ignore = "Remove to run test case")>]
-    [<TestCase("100", ExpectedResult = 4, Ignore = "Remove to run test case")>]
-    [<TestCase("1001", ExpectedResult = 9, Ignore = "Remove to run test case")>]
-    [<TestCase("11010", ExpectedResult = 26, Ignore = "Remove to run test case")>]
-    [<TestCase("10001101000", ExpectedResult = 1128, Ignore = "Remove to run test case")>]
-    member tests.Binary_converts_to_decimal(input) =
-        Binary(input).toDecimal()
+[<TestCase("1", ExpectedResult = 1)>]
+[<TestCase("10", ExpectedResult = 2, Ignore = "Remove to run test case")>]
+[<TestCase("11", ExpectedResult = 3, Ignore = "Remove to run test case")>]
+[<TestCase("100", ExpectedResult = 4, Ignore = "Remove to run test case")>]
+[<TestCase("1001", ExpectedResult = 9, Ignore = "Remove to run test case")>]
+[<TestCase("11010", ExpectedResult = 26, Ignore = "Remove to run test case")>]
+[<TestCase("10001101000", ExpectedResult = 1128, Ignore = "Remove to run test case")>]
+let ``Binary converts to decimal`` (input: string) =
+    toDecimal input
 
-    [<TestCase("carrot", Ignore = "Remove to run test case")>]
-    [<TestCase("2", Ignore = "Remove to run test case")>]
-    [<TestCase("5", Ignore = "Remove to run test case")>]
-    [<TestCase("9", Ignore = "Remove to run test case")>]
-    [<TestCase("134678", Ignore = "Remove to run test case")>]
-    [<TestCase("abc10z", Ignore = "Remove to run test case")>]
-    member tests.Invalid_binary_is_decimal_0(input) =
-        Assert.That(Binary(input).toDecimal(), Is.EqualTo(0))
+[<TestCase("carrot", Ignore = "Remove to run test case")>]
+[<TestCase("2", Ignore = "Remove to run test case")>]
+[<TestCase("5", Ignore = "Remove to run test case")>]
+[<TestCase("9", Ignore = "Remove to run test case")>]
+[<TestCase("134678", Ignore = "Remove to run test case")>]
+[<TestCase("abc10z", Ignore = "Remove to run test case")>]
+let ``Invalid binary is decimal 0`` (input: string) =
+    Assert.That(toDecimal input, Is.EqualTo(0))
--- exercises/binary/Example.fs
+++ exercises/binary/Example.fs
@@ -1,14 +1,13 @@
 ﻿module Binary
 
-type Binary(input: string) =
-    let rec toDecimalLoop acc input = 
-        if input = "" then acc
-        else 
-            let head = input.Chars 0
-            let tail = input.Substring 1
-            match head with
-                | '0' -> toDecimalLoop (acc * 2) tail
-                | '1' -> toDecimalLoop (acc * 2 + 1) tail
-                | _   -> 0   
+let isValid char = 
+    match char with
+    | '0' | '1' -> true
+    | _  -> false
 
-    member this.toDecimal() = toDecimalLoop 0 input
+let charToDecimal char = (int)char - (int)'0'
+
+let toDecimal(input: string) = 
+    let chars = input.ToCharArray()
+    if Array.forall isValid chars then Array.fold (fun acc c -> acc * 2 + charToDecimal c) 0 chars
+    else 0
