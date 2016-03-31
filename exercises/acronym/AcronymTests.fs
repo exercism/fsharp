d7a60f70cf9acca7109fc0e76b2de3df0d6cfdbc
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:19:20 +0200

    Convert acronym exercise to idiomatic F#

--- exercises/acronym/AcronymTests.fs
+++ exercises/acronym/AcronymTests.fs
@@ -3,17 +3,15 @@ module AcronymTests
 open NUnit.Framework
 open Acronym
 
-type AcronymTests() =
-
-    [<Test>]
-    member tests.Empty_string_abbreviated_to_empty_string() =
-        Assert.That(Acronym().abbreviate(""), Is.EqualTo(""))
+[<Test>]
+let ``Empty string abbreviated to empty string`` () =
+    Assert.That(acronym "", Is.EqualTo(""))
         
-    [<TestCase("Portable Network Graphics", ExpectedResult = "PNG", Ignore = "Remove to run test case")>]
-    [<TestCase("Ruby on Rails", ExpectedResult = "ROR", Ignore = "Remove to run test case")>]
-    [<TestCase("HyperText Markup Language", ExpectedResult = "HTML", Ignore = "Remove to run test case")>]
-    [<TestCase("First In, First Out", ExpectedResult = "FIFO", Ignore = "Remove to run test case")>]
-    [<TestCase("PHP: Hypertext Preprocessor", ExpectedResult = "PHP", Ignore = "Remove to run test case")>]
-    [<TestCase("Complementary metal-oxide semiconductor", ExpectedResult = "CMOS", Ignore = "Remove to run test case")>]
-    member tests.Phrase_abbreviated_to_acronym(phrase) =
-        Acronym().abbreviate(phrase)
+[<TestCase("Portable Network Graphics", ExpectedResult = "PNG", Ignore = "Remove to run test case")>]
+[<TestCase("Ruby on Rails", ExpectedResult = "ROR", Ignore = "Remove to run test case")>]
+[<TestCase("HyperText Markup Language", ExpectedResult = "HTML", Ignore = "Remove to run test case")>]
+[<TestCase("First In, First Out", ExpectedResult = "FIFO", Ignore = "Remove to run test case")>]
+[<TestCase("PHP: Hypertext Preprocessor", ExpectedResult = "PHP", Ignore = "Remove to run test case")>]
+[<TestCase("Complementary metal-oxide semiconductor", ExpectedResult = "CMOS", Ignore = "Remove to run test case")>]
+let ``Phrase abbreviated to acronym`` (phrase: string) =
+    acronym phrase
--- exercises/acronym/Example.fs
+++ exercises/acronym/Example.fs
@@ -3,9 +3,8 @@
 open System
 open System.Text.RegularExpressions
 
-type Acronym() =
-    member this.abbreviate(phrase:string) =  
-        let acronymChar = Char.ToUpperInvariant << Seq.head
-        let words = Regex.Matches(phrase, "[A-Z]+[a-z]*|[a-z]+")
-        let chars = [|for word in words do yield word.Value |> acronymChar|]        
-        new String(chars)
+let acronym (phrase:string) =  
+    let acronymChar = Char.ToUpperInvariant << Seq.head
+    let words = Regex.Matches(phrase, "[A-Z]+[a-z]*|[a-z]+")
+    let chars = [|for word in words do yield word.Value |> acronymChar|]        
+    new String(chars)
