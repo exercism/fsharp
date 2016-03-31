b5c2f4d3710c08733a6f641eddbdcf14ffbc37b7
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:25:10 +0200

    Convert rna-transcription exercise to idiomatic F#

--- exercises/rna-transcription/Example.fs
+++ exercises/rna-transcription/Example.fs
@@ -1,11 +1,9 @@
 ﻿module Complement
 
-type Complement() =
+let rnaToDna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('U', 'A'); ('A', 'T')]
+let dnaToRna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('T', 'A'); ('A', 'U')]
 
-    let rnaToDna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('U', 'A'); ('A', 'T')]
-    let dnaToRna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('T', 'A'); ('A', 'U')]
+let transpose (input:string) map = new string(input.ToCharArray() |> Array.map (fun c -> Map.find c map))
 
-    let transpose (input:string) map = new string(input.ToCharArray() |> Array.map (fun c -> Map.find c map))
-
-    member this.ofDna(dna:string) = transpose dna dnaToRna
-    member this.ofRna(rna:string) = transpose rna rnaToDna
+let toRna (dna:string) = transpose dna dnaToRna
+let toDna (rna:string) = transpose rna rnaToDna
--- exercises/rna-transcription/RNATranscriptionTests.fs
+++ exercises/rna-transcription/RNATranscriptionTests.fs
@@ -2,54 +2,52 @@
 
 open NUnit.Framework
 open Complement
-
-type RNATranscriptionTest() =
     
-    [<Test>]
-    member tests.Rna_complement_of_cytosine_is_guanine() =
-        Assert.That(Complement().ofDna("C"), Is.EqualTo("G"))
+[<Test>]
+let ``Rna complement of cytosine is guanine`` () =
+    Assert.That(toRna "C", Is.EqualTo("G"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Rna_complement_of_guanine_is_cytosine() =
-        Assert.That(Complement().ofDna("G"), Is.EqualTo("C"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rna complement of guanine is cytosine`` () =
+    Assert.That(toRna "G", Is.EqualTo("C"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Rna_complement_of_thymine_is_adenine() =
-        Assert.That(Complement().ofDna("T"), Is.EqualTo("A"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rna complement of thymine is adenine`` () =
+    Assert.That(toRna "T", Is.EqualTo("A"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Rna_complement_of_adenine_is_uracil() =
-        Assert.That(Complement().ofDna("A"), Is.EqualTo("U"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rna complement of adenine is uracil`` () =
+    Assert.That(toRna "A", Is.EqualTo("U"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Rna_complement() =
-        Assert.That(Complement().ofDna("ACGTGGTCTTAA"), Is.EqualTo("UGCACCAGAAUU"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rna complement`` () =
+    Assert.That(toRna "ACGTGGTCTTAA", Is.EqualTo("UGCACCAGAAUU"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Dna_complement_of_cytosine_is_guanine() =
-        Assert.That(Complement().ofRna("C"), Is.EqualTo("G"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Dna complement of cytosine is guanine`` () =
+    Assert.That(toDna "C", Is.EqualTo("G"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Dna_complement_of_guanine_is_cytosine() =
-        Assert.That(Complement().ofRna("G"), Is.EqualTo("C"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Dna complement of guanine is cytosine`` () =
+    Assert.That(toDna "G", Is.EqualTo("C"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Dna_complement_of_uracil_is_adenine() =
-        Assert.That(Complement().ofRna("U"), Is.EqualTo("A"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Dna complement of uracil is adenine`` () =
+    Assert.That(toDna "U", Is.EqualTo("A"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Dna_complement_of_adenine_is_thymine() =
-        Assert.That(Complement().ofRna("A"), Is.EqualTo("T"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Dna complement of adenine is thymine`` () =
+    Assert.That(toDna "A", Is.EqualTo("T"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Dna_complement() =
-        Assert.That(Complement().ofRna("UGAACCCGACAUG"), Is.EqualTo("ACTTGGGCTGTAC"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Dna complement`` () =
+    Assert.That(toDna "UGAACCCGACAUG", Is.EqualTo("ACTTGGGCTGTAC"))
