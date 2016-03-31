c2c26fca636b1767e98fedaec2fd40e93efdbbd6
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:27:30 +0200

    Convert simple-linked-list exercise to idiomatic F#

--- exercises/simple-linked-list/SimpleLinkedListTest.fs
+++ exercises/simple-linked-list/SimpleLinkedListTest.fs
@@ -5,49 +5,49 @@ open NUnit.Framework
 open SimpleLinkedList
 
 [<Test>]
-let ``Empty list``() =
+let ``Empty list`` () =
     let list = nil
     Assert.That(isNil list, Is.True)
 
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Single item list value``() =
+let ``Single item list value`` () =
     let list = create 1 nil
     Assert.That(datum list, Is.EqualTo(1))
         
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Single item list has no next item``() =
+let ``Single item list has no next item`` () =
     let list = create 1 nil
     Assert.That(next list |> isNil, Is.True)
         
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Two item list first value``() =
+let ``Two item list first value`` () =
     let list = create 2 (create 1 nil)
     Assert.That(datum list, Is.EqualTo(2))
     
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Two item list second value``() =
+let ``Two item list second value`` () =
     let list = create 2 (create 1 nil)
     Assert.That(next list |> datum, Is.EqualTo(1))
     
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``Two item list second item has no next``() =
+let ``Two item list second item has no next`` () =
     let list = create 2 (create 1 nil)
     Assert.That(next list |> next |> isNil, Is.True)
         
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``To list``() =
+let ``To list`` () =
     let values = create 2 (create 1 nil) |> toList
     Assert.That(values, Is.EqualTo([2; 1]))
         
 [<Test>]
 [<Ignore("Remove to run test")>]
-let ``From list``() =
+let ``From list`` () =
     let list = fromList [11; 7; 5; 3; 2]
     Assert.That(list |> datum, Is.EqualTo(11))
     Assert.That(list |> next |> datum, Is.EqualTo(7))
