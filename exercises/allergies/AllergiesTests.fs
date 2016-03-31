9571e5f89b756c9cca00d4037adeb556cb75f5ef
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:19:28 +0200

    Convert allergies exercise to idiomatic F#

--- exercises/allergies/AllergiesTests.fs
+++ exercises/allergies/AllergiesTests.fs
@@ -4,83 +4,71 @@ open System
 open NUnit.Framework
 open Allergies
 
-type AllergiesTests() =
+[<Test>]
+let ``No allergies means not allergic`` () =
+    Assert.That(allergicTo Allergen.Peanuts 0, Is.False)
+    Assert.That(allergicTo Allergen.Cats 0, Is.False)
+    Assert.That(allergicTo Allergen.Strawberries 0, Is.False)
 
-    [<Test>]
-    member tests.No_allergies_means_not_allergic() =
-        let allergies = Allergies(0);
-        Assert.That(allergies.allergicTo(Allergen.Peanuts), Is.False);
-        Assert.That(allergies.allergicTo(Allergen.Cats), Is.False);
-        Assert.That(allergies.allergicTo(Allergen.Strawberries), Is.False)
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to eggs`` () =
+    Assert.That(allergicTo Allergen.Eggs 1, Is.True)
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_eggs() =
-        let allergies = Allergies(1);
-        Assert.That(allergies.allergicTo(Allergen.Eggs), Is.True)
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to eggs in addition to other stuff`` () =
+    Assert.That(allergicTo Allergen.Eggs 5, Is.True)
+    Assert.That(allergicTo Allergen.Shellfish 5, Is.True)
+    Assert.That(allergicTo Allergen.Strawberries 5, Is.False)
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_eggs_in_addition_to_other_stuff() =
-        let allergies = Allergies(5);
-        Assert.That(allergies.allergicTo(Allergen.Eggs), Is.True);
-        Assert.That(allergies.allergicTo(Allergen.Shellfish), Is.True);
-        Assert.That(allergies.allergicTo(Allergen.Strawberries), Is.False)
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``No allergies at all`` () =
+    Assert.That(allergies 0, Is.Empty)
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.No_allergies_at_all() =
-        let allergies = Allergies(0);
-        Assert.That(allergies.list(), Is.Empty)
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to just eggs`` () =
+    Assert.That(allergies 1, Is.EqualTo([Allergen.Eggs]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_just_eggs() =
-        let allergies = Allergies(1);
-        Assert.That(allergies.list(), Is.EqualTo([Allergen.Eggs]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to just peanuts`` () =
+    Assert.That(allergies 2, Is.EqualTo([Allergen.Peanuts]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_just_peanuts() =
-        let allergies = Allergies(2);
-        Assert.That(allergies.list(), Is.EqualTo([Allergen.Peanuts]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to eggs and peanuts`` () =
+    Assert.That(allergies 3, Is.EqualTo([Allergen.Eggs; Allergen.Peanuts]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_eggs_and_peanuts() =
-        let allergies = Allergies(3);
-        Assert.That(allergies.list(), Is.EqualTo([Allergen.Eggs; Allergen.Peanuts]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to lots of stuff`` () =
+    Assert.That(allergies 248,
+        Is.EqualTo([Allergen.Strawberries; Allergen.Tomatoes; Allergen.Chocolate; Allergen.Pollen; Allergen.Cats]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_lots_of_stuff() =
-        let allergies = Allergies(248);
-        Assert.That(allergies.list(),
-            Is.EqualTo([Allergen.Strawberries; Allergen.Tomatoes; Allergen.Chocolate; Allergen.Pollen; Allergen.Cats]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Allergic to everything`` () =
+    Assert.That(allergies 255,
+        Is.EqualTo([Allergen.Eggs;
+                    Allergen.Peanuts;
+                    Allergen.Shellfish;
+                    Allergen.Strawberries;
+                    Allergen.Tomatoes;
+                    Allergen.Chocolate;
+                    Allergen.Pollen;
+                    Allergen.Cats]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Allergic_to_everything() =
-        let allergies = Allergies(255);
-        Assert.That(allergies.list(),
-            Is.EqualTo([Allergen.Eggs;
-                        Allergen.Peanuts;
-                        Allergen.Shellfish;
-                        Allergen.Strawberries;
-                        Allergen.Tomatoes;
-                        Allergen.Chocolate;
-                        Allergen.Pollen;
-                        Allergen.Cats]))
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Ignore_non_allergen_score_parts() =
-        let allergies = Allergies(509);
-        Assert.That(allergies.list(),
-            Is.EqualTo([Allergen.Eggs;
-                        Allergen.Shellfish;
-                        Allergen.Strawberries;
-                        Allergen.Tomatoes;
-                        Allergen.Chocolate;
-                        Allergen.Pollen;
-                        Allergen.Cats]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Ignore non allergen score parts`` () =
+    Assert.That(allergies 509,
+        Is.EqualTo([Allergen.Eggs;
+                    Allergen.Shellfish;
+                    Allergen.Strawberries;
+                    Allergen.Tomatoes;
+                    Allergen.Chocolate;
+                    Allergen.Pollen;
+                    Allergen.Cats]))
