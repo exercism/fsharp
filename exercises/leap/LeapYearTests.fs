37d3c249194a5175a494cf3f9c3259d3c724ea14
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:23:18 +0200

    Convert leap exercise to idiomatic F#

--- exercises/leap/LeapYearTests.fs
+++ exercises/leap/LeapYearTests.fs
@@ -2,24 +2,22 @@
 
 open NUnit.Framework
 open LeapYear
-
-type LeapYearTests() =
-
-    [<Test>]
-    member x.``Is 1996 a valid leap year``() =
-        Assert.That(LeapYear.isLeap 1996, Is.True)
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member x.``Is 1997 an invalid leap year``() =
-        Assert.That(LeapYear.isLeap 1997, Is.False)
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member x.``Is the turn of the 20th century an invalid leap year``() =
-        Assert.That(LeapYear.isLeap 1900, Is.False)
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member x.``Is the turn of the 25th century a valid leap year``() =
-        Assert.That(LeapYear.isLeap 2400, Is.True)
+    
+[<Test>]
+let ``Is 1996 a valid leap year`` () = 
+    Assert.That(isLeapYear 1996, Is.True)
+    
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Is 1997 an invalid leap year`` () = 
+    Assert.That(isLeapYear 1997, Is.False)
+    
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Is the turn of the 20th century an invalid leap year`` () = 
+    Assert.That(isLeapYear 1900, Is.False)
+    
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Is the turn of the 25th century a valid leap year`` () = 
+    Assert.That(isLeapYear 2400, Is.True)
