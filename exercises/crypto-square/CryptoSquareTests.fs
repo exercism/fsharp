45e3d2a8e6342044a4f0367dd7b5d4021d7bfaa2
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:20:54 +0200

    Convert crypto-square exercise to idiomatic F#

--- exercises/crypto-square/CryptoSquareTests.fs
+++ exercises/crypto-square/CryptoSquareTests.fs
@@ -3,81 +3,66 @@ module CryptoSquareTests
 open NUnit.Framework
 open CryptoSquare
 
-type CryptoSquareTests() =
+[<Test>]
+let ``Strange characters are stripped during normalization`` () =
+    Assert.That(normalizePlaintext "s#$%^&plunk", Is.EqualTo("splunk"))
 
-    [<Test>]
-    member tests.Strange_characters_are_stripped_during_normalization() =
-        let crypto = new CryptoSquare("s#$%^&plunk")
-        Assert.That<string>(crypto.normalizePlaintext, Is.EqualTo("splunk"))
+[<Test>]
+[<Ignore("Remove to run test")>]   
+let ``Letters are lowercased during normalization`` () =
+    Assert.That(normalizePlaintext "WHOA HEY!", Is.EqualTo("whoahey"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Letters_are_lowercased_during_normalization() =
-        let crypto = new CryptoSquare("WHOA HEY!")
-        Assert.That<string>(crypto.normalizePlaintext, Is.EqualTo("whoahey"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Numbers are kept during normalization`` () =
+    Assert.That(normalizePlaintext "1, 2, 3, GO!", Is.EqualTo("123go"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Numbers_are_kept_during_normalization() =
-        let crypto = new CryptoSquare("1, 2, 3, GO!")
-        Assert.That<string>(crypto.normalizePlaintext, Is.EqualTo("123go"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Smallest square size is 2`` () =
+    Assert.That(size "1234", Is.EqualTo(2))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Smallest_square_size_is_2() =
-        let crypto = new CryptoSquare("1234")
-        Assert.That<int>(crypto.size, Is.EqualTo(2))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Size of text whose length is a perfect square is its square root`` () =
+    Assert.That(size "123456789", Is.EqualTo(3))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Size_of_text_whose_length_is_a_perfect_square_is_its_square_root() =
-        let crypto = new CryptoSquare("123456789")
-        Assert.That<int>(crypto.size, Is.EqualTo(3))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Size of text whose length is not a perfect square is next biggest square root`` () =
+    Assert.That(size "123456789abc", Is.EqualTo(4))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Size_of_text_whose_length_is_not_a_perfect_square_is_next_biggest_square_root() =
-        let crypto = new CryptoSquare("123456789abc")
-        Assert.That<int>(crypto.size, Is.EqualTo(4))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Size is determined by normalized text`` () =
+    Assert.That(size "Oh hey, this is nuts!", Is.EqualTo(4))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Size_is_determined_by_normalized_text() =
-        let crypto = new CryptoSquare("Oh hey, this is nuts!")
-        Assert.That<int>(crypto.size, Is.EqualTo(4))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Segments are split by square size`` () =
+    Assert.That(plaintextSegments "Never vex thine heart with idle woes", Is.EqualTo(["neverv"; "exthin"; "eheart"; "withid"; "lewoes"]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Segments_are_split_by_square_size() =
-        let crypto = new CryptoSquare("Never vex thine heart with idle woes")
-        Assert.That(crypto.plaintextSegments(), Is.EqualTo(["neverv"; "exthin"; "eheart"; "withid"; "lewoes"]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Segments are split by square size until text runs out`` () =
+    Assert.That(plaintextSegments "ZOMG! ZOMBIES!!!", Is.EqualTo(["zomg"; "zomb"; "ies"]))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Segments_are_split_by_square_size_until_text_runs_out() =
-        let crypto = new CryptoSquare("ZOMG! ZOMBIES!!!")
-        Assert.That(crypto.plaintextSegments(), Is.EqualTo(["zomg"; "zomb"; "ies"]))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Ciphertext combines text by column`` () =
+    Assert.That(ciphertext "First, solve the problem. Then, write the code.", Is.EqualTo("foeewhilpmrervrticseohtottbeedshlnte"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Ciphertext_combines_text_by_column() =
-        let crypto = new CryptoSquare("First, solve the problem. Then, write the code.")
-        Assert.That(crypto.ciphertext(), Is.EqualTo("foeewhilpmrervrticseohtottbeedshlnte"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Ciphertext skips cells with no text`` () =
+    Assert.That(ciphertext "Time is an illusion. Lunchtime doubly so.", Is.EqualTo("tasneyinicdsmiohooelntuillibsuuml"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Ciphertext_skips_cells_with_no_text() =
-        let crypto = new CryptoSquare("Time is an illusion. Lunchtime doubly so.")
-        Assert.That(crypto.ciphertext(), Is.EqualTo("tasneyinicdsmiohooelntuillibsuuml"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Normalized ciphertext is split by 5`` () =
+    Assert.That(normalizeCiphertext "Vampires are people too!", Is.EqualTo("vrel aepe mset paoo irpo"))
 
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Normalized_ciphertext_is_split_by_5() =
-        let crypto = new CryptoSquare("Vampires are people too!")
-        Assert.That(crypto.normalizeCiphertext(), Is.EqualTo("vrel aepe mset paoo irpo"))
-
-    [<Test>]
-    [<Ignore("Remove to run test")>]
-    member tests.Normalized_ciphertext_not_exactly_divisible_by_5_spills_into_a_smaller_segment() =
-        let crypto = new CryptoSquare("Madness, and then illumination.")
-        Assert.That(crypto.normalizeCiphertext(), Is.EqualTo("msemo aanin dnin ndla etlt shui"))
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Normalized ciphertext not exactly divisible by 5 spills into a smaller segment`` () =
+    Assert.That(normalizeCiphertext "Madness, and then illumination.", Is.EqualTo("msemo aanin dnin ndla etlt shui"))
--- exercises/crypto-square/Example.fs
+++ exercises/crypto-square/Example.fs
@@ -2,24 +2,43 @@
 
 open System
 
-type CryptoSquare(input) = 
+let charsToString chars = new String(chars |> Array.ofSeq)
 
-    let charsToString chars = new String(chars |> Array.ofSeq)
+let chunksOfSize n seq = 
+    seq 
+    |> Seq.mapi(fun i x -> i / n, x)
+    |> Seq.groupBy fst
+    |> Seq.map (fun (_, g) -> g |> Seq.map snd |> charsToString)
 
-    let chunksOfSize n seq = 
-        seq 
-        |> Seq.mapi(fun i x -> i / n, x)
-        |> Seq.groupBy fst
-        |> Seq.map (fun (_, g) -> g |> Seq.map snd |> charsToString)
+let transpose seq = 
+    seq
+    |> Seq.collect(fun s -> s |> Seq.mapi(fun i e -> (i, e)))
+    |> Seq.groupBy(fst)
+    |> Seq.map(fun (_, s) -> s |> Seq.map snd |> charsToString)    
 
-    let transpose seq = 
-        seq
-        |> Seq.collect(fun s -> s |> Seq.mapi(fun i e -> (i, e)))
-        |> Seq.groupBy(fst)
-        |> Seq.map(fun (_, s) -> s |> Seq.map snd |> charsToString)    
+let normalizePlaintext (input: string) = seq { for c in input do if Char.IsLetterOrDigit c then yield Char.ToLowerInvariant c } |> charsToString
 
-    member this.size() = this.normalizePlaintext() |> String.length |> float |> Math.Sqrt |> ceil |> int
-    member this.normalizePlaintext() = seq { for c in input do if Char.IsLetterOrDigit c then yield Char.ToLowerInvariant c } |> charsToString
-    member this.plaintextSegments() = chunksOfSize (this.size()) (this.normalizePlaintext()) |> List.ofSeq
-    member this.ciphertext() = this.plaintextSegments() |> transpose |> String.concat ""
-    member this.normalizeCiphertext() = this.plaintextSegments() |> transpose |> String.concat " "
+let size (input: string) = 
+    input 
+    |> normalizePlaintext 
+    |> String.length 
+    |> float 
+    |> Math.Sqrt 
+    |> ceil 
+    |> int
+
+let plaintextSegments (input: string) = 
+    chunksOfSize (size input) (normalizePlaintext input) 
+    |> List.ofSeq
+
+let ciphertext (input: string) = 
+    input 
+    |> plaintextSegments 
+    |> transpose 
+    |> String.concat ""
+
+let normalizeCiphertext (input: string) = 
+    input 
+    |> plaintextSegments 
+    |> transpose 
+    |> String.concat " "
