067e506931c08078b294ff19489c5e9940671ea1
Erik Schierboom <erik_schierboom@hotmail.com>
2016-03-31 09:27:58 +0200

    Convert sum-of-multiples exercise to idiomatic F#

--- exercises/sum-of-multiples/Example.fs
+++ exercises/sum-of-multiples/Example.fs
@@ -3,4 +3,6 @@
 let sumOfMultiples numbers upperBound =
     let isMultiple x = numbers |> List.exists (fun y -> x % y = 0) 
     
-    [1 .. upperBound - 1] |> List.filter isMultiple |> List.sum
+    [1 .. upperBound - 1] 
+    |> List.filter isMultiple 
+    |> List.sum
