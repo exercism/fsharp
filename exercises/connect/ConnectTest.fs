b049649fba283dee7373df4842fd5ab91b8e8c15
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-05 13:46:17 +0200

    Add connect exercise

---
+++ exercises/connect/ConnectTest.fs
@@ -0,0 +1,98 @@
+﻿module ConnectTest
+
+open NUnit.Framework
+open System
+
+open Connect
+
+let makeBoard (board: string list) = 
+    board
+    |> List.map (fun x -> x.Replace(" ", ""))
+    |> List.reduce (fun x y -> x + "\n" + y)
+
+[<Test>]
+let ``Empty board has no winner`` () =
+    let lines = [". . . . .    ";
+                 " . . . . .   ";
+                 "  . . . . .  ";
+                 "   . . . . . ";
+                 "    . . . . ."]
+    let board = makeBoard lines
+    Assert.That(resultFor board, Is.EqualTo(None))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``1x1 board with black stone`` () =
+    let lines = ["X"]
+    let board = makeBoard lines  
+    Assert.That(resultFor board, Is.EqualTo(Some Black))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``1x1 board with white stone`` () =
+    let lines = ["O"]
+    let board = makeBoard lines  
+    Assert.That(resultFor board, Is.EqualTo(Some White))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Convoluted path`` () =
+    let lines = [". X X . .    ";
+                 " X . X . X   ";
+                 "  . X . X .  ";
+                 "   . X X . . ";
+                 "    O O O O O"]
+    let board = makeBoard lines  
+    Assert.That(resultFor board, Is.EqualTo(Some Black))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rectangle, black wins`` () =
+    let lines = [". O . .    ";
+                 " O X X X   ";
+                 "  O X O .  ";
+                 "   X X O X ";
+                 "    . O X ."]
+    let board = makeBoard lines  
+    Assert.That(resultFor board, Is.EqualTo(Some Black))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Rectangle, white wins`` () =
+    let lines = [". O . .    ";
+                 " O X X X   ";
+                 "  O O O .  ";
+                 "   X X O X ";
+                 "    . O X ."]
+    let board = makeBoard lines  
+    Assert.That(resultFor board, Is.EqualTo(Some White))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Spiral, black wins`` () =
+    let lines = ["OXXXXXXXX";
+                 "OXOOOOOOO";
+                 "OXOXXXXXO";
+                 "OXOXOOOXO";
+                 "OXOXXXOXO";
+                 "OXOOOXOXO";
+                 "OXXXXXOXO";
+                 "OOOOOOOXO";
+                 "XXXXXXXXO"]
+    let board = makeBoard lines
+    Assert.That(resultFor board, Is.EqualTo(Some Black))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Spiral, nobody wins`` () =
+    let lines = ["OXXXXXXXX";
+                 "OXOOOOOOO";
+                 "OXOXXXXXO";
+                 "OXOXOOOXO";
+                 "OXOX.XOXO";
+                 "OXOOOXOXO";
+                 "OXXXXXOXO";
+                 "OOOOOOOXO";
+                 "XXXXXXXXO"]
+    let board = makeBoard lines
+    Assert.That(resultFor board, Is.EqualTo(None))
---
+++ exercises/connect/Example.fs
@@ -0,0 +1,63 @@
+﻿module Connect
+
+type Color = Black | White
+type Coordinate = int * int
+
+type Board = Color option [,]
+
+let cols (board:Board) = board.GetLength(0)
+let rows (board:Board) = board.GetLength(1)  
+
+let isValidCoordinate board (col, row) = row >= 0 && row < rows board && col >= 0 && col < cols board
+
+let hasColor color (board:Board) (col, row) = Array2D.get board col row = Some color
+
+let adjacent color board (col, row) = 
+    let valid coord = isValidCoordinate board coord && hasColor color board coord
+    let coords = [(col + 1, row - 1);
+                  (col,     row - 1); 
+                  (col - 1, row); 
+                  (col + 1, row); 
+                  (col - 1, row + 1);
+                  (col,     row + 1)]
+    List.filter valid coords
+
+let rec validPath color stop board processed coord =
+    if stop board coord then true
+    else
+        let next = adjacent color board coord
+        let notVisited = Set.difference (next |> Set.ofList) processed
+
+        if Set.isEmpty notVisited then false
+        else Set.exists (fun nextCoord -> validPath color stop board (Set.add coord processed) nextCoord) notVisited
+
+let isWhiteStop board (_, row) = row = rows board - 1
+let isBlackStop board (col, _) = col = cols board - 1
+
+let whiteStart board = [for col in 0..cols board - 1 do if hasColor White board (col, 0) then yield (col, 0)] |> Set.ofList
+let blackStart board = [for row in 0..rows board - 1 do if hasColor Black board (0, row) then yield (0, row)] |> Set.ofList
+
+let colorWins color stop start board = Set.exists (validPath color stop board Set.empty) (start board)
+
+let whiteWins = colorWins White isWhiteStop whiteStart
+let blackWins = colorWins Black isBlackStop blackStart
+
+let charToColor c = 
+    match c with
+    | 'O' -> Some White
+    | 'X' -> Some Black
+    | _   -> None
+
+let mkBoard (input: string) = 
+    let split = input.Split('\n')
+    let rows' = Array.length split
+    let cols' = Seq.length (Array.head split)
+    let cellColor col row = split |> Array.item row |> Seq.item col |> charToColor
+    
+    Array2D.init cols' rows' cellColor
+
+let resultFor input =
+    let board = mkBoard input
+    if whiteWins board then Some White
+    elif blackWins board then Some Black
+    else None
