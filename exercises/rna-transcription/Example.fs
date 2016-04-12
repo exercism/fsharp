f51e785c101d320f6dd5c972bdb98204ec4bb60d
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-12 14:41:41 +0200

    Fix rna-transcription exercise. Closes #99

--- exercises/rna-transcription/Example.fs
+++ exercises/rna-transcription/Example.fs
@@ -1,9 +1,7 @@
-﻿module Complement
+﻿module RNATranscription
 
-let rnaToDna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('U', 'A'); ('A', 'T')]
 let dnaToRna = Map.ofSeq [('G', 'C'); ('C', 'G'); ('T', 'A'); ('A', 'U')]
 
 let transpose (input:string) map = new string(input.ToCharArray() |> Array.map (fun c -> Map.find c map))
 
 let toRna (dna:string) = transpose dna dnaToRna
-let toDna (rna:string) = transpose rna rnaToDna
