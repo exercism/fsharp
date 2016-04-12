d3c85a0694393a380a58b6d34ae2bca9463b6a42
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-12 14:33:48 +0200

    Fix scrabble-score. Closes #97

--- exercises/scrabble-score/Example.fs
+++ exercises/scrabble-score/Example.fs
@@ -3,7 +3,7 @@
 open System
 
 let letterScores = 
-    [("AEIOULNRST", 1); ("DG", 2); ("BCMP", 3); ("FHVWY", 4); ("K", 5); ("JX", 9); ("QZ", 10)] 
+    [("AEIOULNRST", 1); ("DG", 2); ("BCMP", 3); ("FHVWY", 4); ("K", 5); ("JX", 8); ("QZ", 10)] 
     |> List.map (fun (letters, score) -> letters |> Seq.map (fun letter -> (letter, score))) 
     |> Seq.concat 
     |> Map.ofSeq
