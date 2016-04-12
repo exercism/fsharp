e2684b7e415599d85cfc918a34ca52b0c80369c9
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-12 14:05:24 +0200

    Add proverb exercise

---
+++ exercises/proverb/Example.fs
@@ -0,0 +1,13 @@
+﻿module Proverb
+
+let subjects = ["nail"; "shoe"; "horse"; "rider"; "message"; "battle"; "kingdom"]
+
+let line number = 
+    match number with
+    | 7 -> "And all for the want of a horseshoe nail."
+    | _ -> sprintf "For want of a %s the %s was lost." (List.item (number - 1) subjects) (List.item number subjects) 
+
+let proverb = 
+    [1..7]
+    |> List.map line
+    |> List.reduce (fun x y -> x + "\n" + y)
---
+++ exercises/proverb/ProverbTest.fs
@@ -0,0 +1,34 @@
+﻿module ProverbTest
+
+open NUnit.Framework
+
+open Proverb
+
+[<Test>]
+let ``Line one`` () =
+    Assert.That(line 1, Is.EqualTo("For want of a nail the shoe was lost."))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Line four`` () =
+    Assert.That(line 4, Is.EqualTo("For want of a rider the message was lost."))
+    
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Line seven`` () =
+    Assert.That(line 7, Is.EqualTo("And all for the want of a horseshoe nail."))
+    
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Proverb`` () =
+    let expected =
+        ["For want of a nail the shoe was lost.";
+         "For want of a shoe the horse was lost.";
+         "For want of a horse the rider was lost.";
+         "For want of a rider the message was lost.";
+         "For want of a message the battle was lost.";
+         "For want of a battle the kingdom was lost.";
+         "And all for the want of a horseshoe nail."]
+        |> List.reduce (fun x y -> x + "\n" + y)
+
+    Assert.That(proverb, Is.EqualTo(expected))
