4538d861a9b64ad493ad5b3da18fa8b0f6fc202d
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-29 16:38:56 +0200

    Add house exercise

---
+++ exercises/house/Example.fs
@@ -0,0 +1,35 @@
+﻿module House
+
+let subjects = 
+    [("house that Jack built", "lay in");
+     ("malt", "ate");
+     ("rat", "killed");
+     ("cat", "worried");
+     ("dog", "tossed");
+     ("cow with the crumpled horn", "milked");
+     ("maiden all forlorn", "kissed");
+     ("man all tattered and torn", "married");
+     ("priest all shaven and shorn", "woke");
+     ("rooster that crowed in the morn", "kept");
+     ("farmer sowing his corn", "belonged to");
+     ("horse and the hound and the horn", "")]
+
+let numberOfBlocks = List.length subjects
+
+let line number index = 
+    let (subject, verb) = List.item (index - 1) subjects
+    let ending = if index = 1 then "." else ""
+    
+    if index = number then "This is the " + subject + ending
+    else "that " + verb + " the " + subject + ending
+    
+let block number = 
+    let lineForBlock = line number
+    [number .. -1 .. 1]
+    |> List.map lineForBlock
+    |> List.reduce (fun x y -> x + "\n" + y)    
+
+let rhyme = 
+    [1..numberOfBlocks]
+    |> List.map block
+    |> List.reduce (fun x y -> x + "\n\n" + y)
---
+++ exercises/house/HouseTest.fs
@@ -0,0 +1,102 @@
+﻿module HouseTest
+
+open NUnit.Framework
+
+open House
+
+let expected = 
+    [
+    "This is the house that Jack built.";
+    "";
+    "This is the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the cow with the crumpled horn";
+    "that tossed the dog"; 
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the man all tattered and torn";
+    "that kissed the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the priest all shaven and shorn";
+    "that married the man all tattered and torn";
+    "that kissed the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the rooster that crowed in the morn";
+    "that woke the priest all shaven and shorn";
+    "that married the man all tattered and torn";
+    "that kissed the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the farmer sowing his corn";
+    "that kept the rooster that crowed in the morn";
+    "that woke the priest all shaven and shorn";
+    "that married the man all tattered and torn";
+    "that kissed the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built.";
+    "";
+    "This is the horse and the hound and the horn";
+    "that belonged to the farmer sowing his corn";
+    "that kept the rooster that crowed in the morn";
+    "that woke the priest all shaven and shorn";
+    "that married the man all tattered and torn";
+    "that kissed the maiden all forlorn";
+    "that milked the cow with the crumpled horn";
+    "that tossed the dog";
+    "that worried the cat";
+    "that killed the rat";
+    "that ate the malt";
+    "that lay in the house that Jack built."]
+    |> List.reduce (fun x y -> x + "\n" + y)
+
+[<Test>]
+let ``Rhyme is correct`` () =
+    Assert.That(rhyme, Is.EqualTo(expected))
