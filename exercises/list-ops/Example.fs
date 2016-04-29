8f178fefb8c73f2c8085cc093c345d0908066e0f
Erik Schierboom <erik_schierboom@hotmail.com>
2016-04-29 17:23:30 +0200

    Add list-ops exercise

---
+++ exercises/list-ops/Example.fs
@@ -0,0 +1,23 @@
+﻿module ListOps
+
+let rec foldl folder state list =
+    match list with
+    | [] -> state
+    | x::xs -> foldl folder (folder state x) xs
+
+let rec foldr folder state list =
+    list
+    |> List.rev
+    |> foldl (fun acc item -> folder item acc) state
+
+let length list = foldl (fun acc _ -> acc + 1) 0 list
+
+let reverse list = foldl (fun acc item -> item :: acc) [] list
+
+let map f list = foldr (fun item acc -> f item :: acc) [] list
+
+let filter f list = foldr (fun item acc -> if f item then item :: acc else acc) [] list
+
+let append xs ys = foldr (fun item acc -> item :: acc) ys xs
+
+let concat xs = foldr append [] xs
---
+++ exercises/list-ops/ListOpsTest.fs
@@ -0,0 +1,127 @@
+﻿module ListOpsTest
+
+open NUnit.Framework
+
+open ListOps
+
+let big = 100000
+let odd x = x % 2 = 1
+
+[<Test>]
+let ``length of empty list`` () =
+    Assert.That(length [], Is.EqualTo(0))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``length of non-empty list`` () =
+    Assert.That(length [1 .. 4], Is.EqualTo(4))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``length of large list`` () =
+    Assert.That(length [1 .. big], Is.EqualTo(big))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``reverse of empty list`` () =
+    Assert.That(reverse [], Is.EqualTo([]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``reverse of non-empty list`` () =
+    Assert.That(reverse [1 .. 100], Is.EqualTo([100 .. -1 .. 1]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``map of empty list`` () =
+    Assert.That(map ((+) 1) [], Is.EqualTo([]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``map of non-empty list`` () =
+    Assert.That(map ((+) 1) [1 .. 2 .. 7], Is.EqualTo([2 .. 2 .. 8]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``filter of empty list`` () =
+    Assert.That(filter id [], Is.EqualTo([]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``filter of normal list`` () =
+    Assert.That(filter odd [1 .. 4], Is.EqualTo([1; 3]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldl of empty list`` () =
+    Assert.That(foldl (+) 0 [], Is.EqualTo(0))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldl of non-empty list`` () =
+    Assert.That(foldl (+) -3 [1 .. 4], Is.EqualTo(7))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldl of huge list`` () =
+    Assert.That(foldl (+) 0 [1 .. big], Is.EqualTo(big * (big + 1) / 2))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldl with non-commutative function`` () =
+    Assert.That(foldl (-) 10 [1 .. 4], Is.EqualTo(0))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldl is not just foldr . flip`` () =
+    Assert.That(foldl (fun acc item -> item :: acc) [] (List.ofSeq "asdf"), Is.EqualTo(List.ofSeq "fdsa"))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldr as id`` () =
+    Assert.That(foldr (fun item acc -> item :: acc) [] [1 .. big], Is.EqualTo([1 .. big]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``foldr as append`` () =
+    Assert.That(foldr (fun item acc -> item :: acc) [100 .. big] [1 .. 99], Is.EqualTo([1 .. big]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``append of empty lists`` () =
+    Assert.That(append [] [], Is.EqualTo([]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``append of empty and non-empty lists`` () =
+    Assert.That(append [] [1 .. 4], Is.EqualTo([1 .. 4]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``append of non-empty and empty lists`` () =
+    Assert.That(append [1 .. 4] [], Is.EqualTo([1 .. 4]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``append of non-empty lists`` () =
+    Assert.That(append [1 .. 3] [4; 5], Is.EqualTo([1 .. 5]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``append of large lists`` () =
+    Assert.That(append [1 .. (big / 2)] [1 + big / 2 .. big], Is.EqualTo([1 .. big]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``concat of no lists`` () =
+    Assert.That(concat [], Is.EqualTo([]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``concat of list of lists`` () =
+    Assert.That(concat [[1; 2]; [3]; []; [4; 5; 6]], Is.EqualTo([1 .. 6]))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``concat of large list of small lists`` () =
+    Assert.That(concat (map (fun x -> [x]) [1 .. big]), Is.EqualTo([1 .. big]))
