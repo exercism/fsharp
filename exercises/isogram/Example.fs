a2c1a7eab0481d513699e47f9d5cdbc33588d696
Erik Schierboom <erik_schierboom@hotmail.com>
2016-05-13 16:21:20 +0200

    Add isogram exercise

---
+++ exercises/isogram/Example.fs
@@ -0,0 +1,10 @@
+﻿module Isogram
+
+open System
+
+let normalize str = str |> Seq.filter Char.IsLetter |> Seq.map Char.ToLower |> Seq.toList
+
+let isogram str = 
+    let normalized = normalize str
+    Seq.length normalized = (normalized |> Seq.distinct |> Seq.length) 
+
