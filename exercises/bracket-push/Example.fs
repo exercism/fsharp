0762b7384e19e24ef5d108dea0e5db0ab9deb78e
Erik Schierboom <erik_schierboom@hotmail.com>
2016-05-18 11:33:46 +0200

    Add bracket-push exercise

---
+++ exercises/bracket-push/Example.fs
@@ -0,0 +1,19 @@
+﻿module BracketPush
+
+open System
+
+let matched (input: string) =
+    let brackets = "[]{}()" |> Set.ofSeq
+    let filtered = input.ToCharArray() |> Array.filter brackets.Contains |> String
+    let replaceMatchingBrackets (str: string) = str.Replace("[]", "").Replace("{}", "").Replace("()", "")
+    
+    let rec loop (remaining: string) =
+        match remaining.Length with
+        | 0 -> true
+        | _  -> 
+            let updated = replaceMatchingBrackets remaining
+            match updated.Length = remaining.Length with
+            | true -> false
+            | false -> loop updated
+        
+    loop filtered
