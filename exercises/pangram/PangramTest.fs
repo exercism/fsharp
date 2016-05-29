2ec4165c1b2252318c17bc585c60397dede33192
Erik Schierboom <erik_schierboom@hotmail.com>
2016-05-29 12:59:30 +0200

    Add new tests for pangram. Closes #129

--- exercises/pangram/PangramTest.fs
+++ exercises/pangram/PangramTest.fs
@@ -20,6 +20,30 @@ let ``Pangram with only lower case`` () =
 let ``Missing character 'x'`` () =
     let input = "a quick movement of the enemy will jeopardize five gunboats"
     Assert.That(isPangram input, Is.False)
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Another missing character 'x'`` () =
+    let input = "the quick brown fish jumps over the lazy dog"
+    Assert.That(isPangram input, Is.False)
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Pangram with underscores`` () =
+    let input = "the_quick_brown_fox_jumps_over_the_lazy_dog"
+    Assert.That(isPangram input, Is.True)
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Pangram with numbers`` () =
+    let input = "the 1 quick brown fox jumps over the 2 lazy dogs"
+    Assert.That(isPangram input, Is.True)
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Missing letters replaced by numbers`` () =
+    let input = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"
+    Assert.That(isPangram input, Is.False)
     
 [<Test>]
 [<Ignore("Remove to run test")>]
