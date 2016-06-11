ad75e932f74e442a4aa12c1005ac917b635f277e
Erik Schierboom <erik_schierboom@hotmail.com>
2016-06-11 13:37:59 +0200

    Add zebra-puzzle exercise

---
+++ exercises/zebra-puzzle/ZebraPuzzleTest.fs
@@ -0,0 +1,16 @@
+﻿module ZebraPuzzleTest
+
+open NUnit.Framework
+
+open ZebraPuzzle
+
+[<Test>]
+let ``Who drinks water?`` () =
+    let solution = solve
+    Assert.That(whoDrinksWater solution, Is.EqualTo(Norwegian))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Who owns the zebra?`` () =
+    let solution = solve
+    Assert.That(whoOwnsZebra solution, Is.EqualTo(Japanese))
