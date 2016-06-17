2e0ef6e7b0df6c561c2b62891263a8efa937aca0
Erik Schierboom <erik_schierboom@hotmail.com>
2016-06-17 15:32:28 +0200

    Made robot-name exercise functional

--- exercises/robot-name/Example.fs
+++ exercises/robot-name/Example.fs
@@ -2,18 +2,21 @@
 
 let random = System.Random()
 
-type Robot() =
-    let letters = ['A'..'Z']
-    let digits = ['0'..'9']
+type Robot = { name: string }
 
-    let NumberOfLetters = 2;
-    let NumberOfDigits = 3    
+let letters = ['A'..'Z']
+let digits = ['0'..'9']
 
-    let takeRandomElements xs length = List.init length (fun _ -> List.item (random.Next(List.length xs)) xs)
-    let generateRandomString chars length = new System.String(takeRandomElements chars length |> List.toArray)
-    let generateLetters() = generateRandomString letters NumberOfLetters
-    let generateDigits() = generateRandomString digits NumberOfDigits
-    let generateName() = generateLetters() + generateDigits()
-    
-    member val Name = generateName() with get, set
-    member this.Reset() = this.Name <- generateName()
+let NumberOfLetters = 2;
+let NumberOfDigits = 3    
+
+let takeRandomElements xs length = List.init length (fun _ -> List.item (random.Next(List.length xs)) xs)
+let generateRandomString chars length = new System.String(takeRandomElements chars length |> List.toArray)
+let generateLetters() = generateRandomString letters NumberOfLetters
+let generateDigits() = generateRandomString digits NumberOfDigits
+let generateName() = generateLetters() + generateDigits()
+
+let mkRobot() = { name = generateName() }
+
+let name robot = robot.name
+let reset _ = mkRobot()
