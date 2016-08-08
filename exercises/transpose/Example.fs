dc97ab50a65f4c88eb9c0c3d60d9b0b5ecdebfdf
Erik Schierboom <erik_schierboom@hotmail.com>
2016-08-08 14:04:12 +0200

    Update transpose exercise to work with strings

--- exercises/transpose/Example.fs
+++ exercises/transpose/Example.fs
@@ -1,6 +1,18 @@
 ﻿module Transpose
 
-let rec transpose = function
-    | (_::_)::_ as M -> List.map List.head M :: transpose (List.map List.tail M)
-    | _ -> []
+let maxLength (input: string[]) = 
+    input
+    |> Array.map (fun x -> x.Length) 
+    |> Array.max
 
+let padRows rows =
+    rows
+    |> Array.mapi (fun i row -> if i < Array.length rows - 1 then rows |> maxLength |> row.PadRight else row)
+
+let transpose (input: string) = 
+    input.Split '\n'
+    |> padRows
+    |> Seq.collect (fun s -> s |> Seq.mapi (fun i e -> (i, e)))
+    |> Seq.groupBy fst
+    |> Seq.map (fun (_, s) -> s |> Seq.map snd |> Seq.toArray |> System.String)
+    |> String.concat "\n"
