86f150be594f364d7d5b75ebc14df094a0bffff4
Erik Schierboom <erik_schierboom@hotmail.com>
2016-09-05 08:23:45 +0200

    Use array in binary search exercise (#211)

--- exercises/binary-search/Example.fs
+++ exercises/binary-search/Example.fs
@@ -2,15 +2,16 @@
 
 let rec binarySearchAux index value =
     function
-    | [] -> None
-    | xs ->   
-        let middle = xs.Length / 2
-        let middleValue = xs.[middle]
+    | [||] -> 
+        None
+    | input ->   
+        let middle = input.Length / 2
+        let middleValue = input.[middle]
         
-        if value < middleValue then binarySearchAux index value xs.[0 .. middle - 1] 
-        elif value > middleValue then binarySearchAux (index + middle + 1) value xs.[middle + 1 ..]
+        if value < middleValue then binarySearchAux index value input.[0 .. middle - 1] 
+        elif value > middleValue then binarySearchAux (index + middle + 1) value input.[middle + 1 ..]
         else Some (index + middle)
 
-let binarySearch list value =
-    if List.sort list <> list then failwith "The input must be an ordered lists"
-    else binarySearchAux 0 value list
+let binarySearch input value =
+    if Array.sort input <> input then failwith "The input must be an ordered lists"
+    else binarySearchAux 0 value input
