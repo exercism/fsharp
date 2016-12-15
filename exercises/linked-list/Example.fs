d28b5f88ff727f3e803c92576a4dd0fcaf5f6023
theprash <theprash@gmail.com>
2016-12-15 18:03:11 +0000

    Change linked-lists tests and example to use mutation. (#248)
    * Change linked-lists tests and example to use mutation. And fix errors in the example.
    
    * Add hints

--- exercises/linked-list/Example.fs
+++ exercises/linked-list/Example.fs
@@ -1,36 +1,43 @@
 module LinkedList
 
 type Element<'a> = { value: 'a; mutable prev: Element<'a> option; mutable next: Element<'a> option }
-type LinkedList<'a> = { first: Element<'a> option; last: Element<'a> option }
+type LinkedList<'a> = { mutable first: Element<'a> option; mutable last: Element<'a> option }
 
-let mkLinkedList = { first = None; last = None }
+let mkLinkedList () = { first = None; last = None }
 
-let addToEmpty newValue linkedList = 
+let addToEmpty newValue linkedList =
     let newElement = { value = newValue; prev = None; next = None }
-    { linkedList with first = Some newElement; last = Some newElement }
+    linkedList.first <- Some newElement
+    linkedList.last <- Some newElement
 
 let pop linkedList =
     match linkedList.last with
     | None -> failwith "Cannot pop from empty list"
-    | Some(x) -> x.value, { linkedList with last = x.prev }
+    | Some oldLast ->
+        linkedList.last <- oldLast.prev
+        linkedList.last |> Option.iter (fun el -> el.next <- None)
+        oldLast.value
 
 let shift linkedList =
     match linkedList.first with
     | None -> failwith "Cannot shift from empty list"
-    | Some(x) -> x.value, { linkedList with first = x.next }
-        
-let push newValue linkedList = 
+    | Some oldFirst ->
+        linkedList.first <- oldFirst.next
+        linkedList.first |> Option.iter (fun el -> el.prev <- None)
+        oldFirst.value
+
+let push newValue linkedList =
     match linkedList.last with
     | None -> addToEmpty newValue linkedList
-    | Some(x) ->         
-        let last = Some { value = newValue; prev = linkedList.last; next = x.next }
-        x.next <- last            
-        { linkedList with last = last }
+    | Some oldLast ->
+        let newLast = Some { value = newValue; prev = linkedList.last; next = None }
+        oldLast.next <- newLast
+        linkedList.last <- newLast
 
-let unshift newValue linkedList =        
+let unshift newValue linkedList =
     match linkedList.first with
     | None -> addToEmpty newValue linkedList
-    | Some(x) ->     
-        let first = Some { value = newValue; prev = x.prev; next = linkedList.first }
-        x.prev <- first
-        { linkedList with first = first }
+    | Some oldFirst ->
+        let newFirst = Some { value = newValue; prev = None; next = linkedList.first }
+        oldFirst.prev <- newFirst
+        linkedList.first <- newFirst
--- exercises/linked-list/LinkedListTest.fs
+++ exercises/linked-list/LinkedListTest.fs
@@ -5,117 +5,110 @@ open LinkedList
 
 [<Test>]
 let ``Push and pop are first in last out order`` () =
-    let linkedList = 
-        mkLinkedList
-        |> push 10
-        |> push 20
+    let linkedList = mkLinkedList ()
+    linkedList |> push 10
+    linkedList |> push 20
 
-    let (val', linkedList') = pop linkedList
-    let (val'', _) = pop linkedList'
+    let val1 = pop linkedList
+    let val2 = pop linkedList
 
-    Assert.That(val', Is.EqualTo(20))
-    Assert.That(val'', Is.EqualTo(10))
+    Assert.That(val1, Is.EqualTo(20))
+    Assert.That(val2, Is.EqualTo(10))
 
-[<Test>]  
-[<Ignore("Remove to run test")>]  
+[<Test>]
+[<Ignore("Remove to run test")>]
 let ``Push and shift are first in first out order`` () =
-    let linkedList = 
-        mkLinkedList
-        |> push 10
-        |> push 20
+    let linkedList = mkLinkedList ()
+    linkedList |> push 10
+    linkedList |> push 20
 
-    let (val', linkedList') = shift linkedList
-    let (val'', _) = shift linkedList'
+    let val1 = shift linkedList
+    let val2 = shift linkedList
 
-    Assert.That(val', Is.EqualTo(10))
-    Assert.That(val'', Is.EqualTo(20))
+    Assert.That(val1, Is.EqualTo(10))
+    Assert.That(val2, Is.EqualTo(20))
 
-[<Test>] 
-[<Ignore("Remove to run test")>]  
+[<Test>]
+[<Ignore("Remove to run test")>]
 let ``Unshift and shift are last in first out order`` () =
-    let linkedList = 
-        mkLinkedList
-        |> unshift 10
-        |> unshift 20
+    let linkedList = mkLinkedList ()
+    linkedList |> unshift 10
+    linkedList |> unshift 20
 
-    let (val', linkedList') = shift linkedList
-    let (val'', _) = shift linkedList'
+    let val1 = shift linkedList
+    let val2 = shift linkedList
 
-    Assert.That(val', Is.EqualTo(20))
-    Assert.That(val'', Is.EqualTo(10))
+    Assert.That(val1, Is.EqualTo(20))
+    Assert.That(val2, Is.EqualTo(10))
 
-[<Test>]    
+[<Test>]
 [<Ignore("Remove to run test")>]
 let ``Unshift and pop are last in last out order`` () =
-    let linkedList = 
-        mkLinkedList
-        |> unshift 10
-        |> unshift 20
+    let linkedList = mkLinkedList ()
+    linkedList |> unshift 10
+    linkedList |> unshift 20
 
-    let (val', linkedList') = pop linkedList
-    let (val'', _) = pop linkedList'
+    let val1 = pop linkedList
+    let val2 = pop linkedList
 
-    Assert.That(val', Is.EqualTo(10))
-    Assert.That(val'', Is.EqualTo(20))
+    Assert.That(val1, Is.EqualTo(10))
+    Assert.That(val2, Is.EqualTo(20))
 
-[<Test>]    
+[<Test>]
 [<Ignore("Remove to run test")>]
 let ``Push and pop can handle multiple values`` () =
-    let linkedList = 
-        mkLinkedList
-        |> push 10
-        |> push 20
-        |> push 30
+    let linkedList = mkLinkedList ()
+    linkedList |> push 10
+    linkedList |> push 20
+    linkedList |> push 30
 
-    let (val', linkedList') = pop linkedList
-    let (val'', linkedList'') = pop linkedList'
-    let (val''', _) = pop linkedList''
+    let val1 = pop linkedList
+    let val2 = pop linkedList
+    let val3 = pop linkedList
 
-    Assert.That(val', Is.EqualTo(30))
-    Assert.That(val'', Is.EqualTo(20))
-    Assert.That(val''', Is.EqualTo(10))
+    Assert.That(val1, Is.EqualTo(30))
+    Assert.That(val2, Is.EqualTo(20))
+    Assert.That(val3, Is.EqualTo(10))
 
-[<Test>]    
+[<Test>]
 [<Ignore("Remove to run test")>]
 let ``Unshift and shift can handle multiple values`` () =
-    let linkedList = 
-        mkLinkedList
-        |> unshift 10
-        |> unshift 20
-        |> unshift 30
+    let linkedList = mkLinkedList ()
+    linkedList |> unshift 10
+    linkedList |> unshift 20
+    linkedList |> unshift 30
 
-    let (val', linkedList') = shift linkedList
-    let (val'', linkedList'') = shift linkedList'
-    let (val''', _) = shift linkedList''
+    let val1 = shift linkedList
+    let val2 = shift linkedList
+    let val3 = shift linkedList
 
-    Assert.That(val', Is.EqualTo(30))
-    Assert.That(val'', Is.EqualTo(20))
-    Assert.That(val''', Is.EqualTo(10))
+    Assert.That(val1, Is.EqualTo(30))
+    Assert.That(val2, Is.EqualTo(20))
+    Assert.That(val3, Is.EqualTo(10))
 
-[<Test>]    
+[<Test>]
 [<Ignore("Remove to run test")>]
 let ``All methods of manipulating the linkedList can be used together`` () =
-    let linkedList = 
-        mkLinkedList
-        |> push 10
-        |> push 20
+    let linkedList = mkLinkedList ()
+    linkedList |> push 10
+    linkedList |> push 20
 
-    let (val', linkedList') = pop linkedList
+    let val1 = pop linkedList
 
-    Assert.That(val', Is.EqualTo(20))
+    Assert.That(val1, Is.EqualTo(20))
 
-    let linkedList'' = push 30 linkedList'
-    let (val''', linkedList''') = shift linkedList''
+    linkedList |> push 30
+    let val2 = shift linkedList
 
-    Assert.That(val''', Is.EqualTo(10))
+    Assert.That(val2, Is.EqualTo(10))
 
-    let linkedList'''' = unshift 40 linkedList'''
-    let linkedList''''' = push 50 linkedList''''
+    linkedList |> unshift 40
+    linkedList |> push 50
 
-    let (val'''''', linkedList'''''') = shift linkedList'''''
-    let (val''''''', linkedList''''''') = pop linkedList''''''
-    let (val'''''''', _) = shift linkedList'''''''
-        
-    Assert.That(val'''''', Is.EqualTo(40))
-    Assert.That(val''''''', Is.EqualTo(50))
-    Assert.That(val'''''''', Is.EqualTo(30))
+    let val3 = shift linkedList
+    let val4 = pop linkedList
+    let val5 = shift linkedList
+
+    Assert.That(val3, Is.EqualTo(40))
+    Assert.That(val4, Is.EqualTo(50))
+    Assert.That(val5, Is.EqualTo(30))
