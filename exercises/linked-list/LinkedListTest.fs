8f25eb12c78cb5fed88d2aad4d364f9da0620fd7
Erik Schierboom <erik_schierboom@hotmail.com>
2017-01-24 10:13:19 +0100

    Revert "Fix #286 - Linked List: change pop and shift to return option"

--- exercises/linked-list/LinkedListTest.fs
+++ exercises/linked-list/LinkedListTest.fs
@@ -4,14 +4,6 @@ open NUnit.Framework
 open LinkedList
 
 [<Test>]
-let ``Pop returns None if the list is empty`` () =
-    let linkedList = mkLinkedList ()
-    let value = pop linkedList
-
-    Assert.That(value, Is.EqualTo(None))
-
-[<Test>]
-[<Ignore("Remove to run test")>]
 let ``Push and pop are first in last out order`` () =
     let linkedList = mkLinkedList ()
     linkedList |> push 10
@@ -20,16 +12,8 @@ let ``Push and pop are first in last out order`` () =
     let val1 = pop linkedList
     let val2 = pop linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 20))
-    Assert.That(val2, Is.EqualTo(Some 10))
-
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Shift returns None if the list is empty`` () =
-    let linkedList = mkLinkedList ()
-    let value = shift linkedList
-
-    Assert.That(value, Is.EqualTo(None))
+    Assert.That(val1, Is.EqualTo(20))
+    Assert.That(val2, Is.EqualTo(10))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -41,8 +25,8 @@ let ``Push and shift are first in first out order`` () =
     let val1 = shift linkedList
     let val2 = shift linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 10))
-    Assert.That(val2, Is.EqualTo(Some 20))
+    Assert.That(val1, Is.EqualTo(10))
+    Assert.That(val2, Is.EqualTo(20))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -54,8 +38,8 @@ let ``Unshift and shift are last in first out order`` () =
     let val1 = shift linkedList
     let val2 = shift linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 20))
-    Assert.That(val2, Is.EqualTo(Some 10))
+    Assert.That(val1, Is.EqualTo(20))
+    Assert.That(val2, Is.EqualTo(10))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -67,8 +51,8 @@ let ``Unshift and pop are last in last out order`` () =
     let val1 = pop linkedList
     let val2 = pop linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 10))
-    Assert.That(val2, Is.EqualTo(Some 20))
+    Assert.That(val1, Is.EqualTo(10))
+    Assert.That(val2, Is.EqualTo(20))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -82,9 +66,9 @@ let ``Push and pop can handle multiple values`` () =
     let val2 = pop linkedList
     let val3 = pop linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 30))
-    Assert.That(val2, Is.EqualTo(Some 20))
-    Assert.That(val3, Is.EqualTo(Some 10))
+    Assert.That(val1, Is.EqualTo(30))
+    Assert.That(val2, Is.EqualTo(20))
+    Assert.That(val3, Is.EqualTo(10))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -98,9 +82,9 @@ let ``Unshift and shift can handle multiple values`` () =
     let val2 = shift linkedList
     let val3 = shift linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 30))
-    Assert.That(val2, Is.EqualTo(Some 20))
-    Assert.That(val3, Is.EqualTo(Some 10))
+    Assert.That(val1, Is.EqualTo(30))
+    Assert.That(val2, Is.EqualTo(20))
+    Assert.That(val3, Is.EqualTo(10))
 
 [<Test>]
 [<Ignore("Remove to run test")>]
@@ -111,12 +95,12 @@ let ``All methods of manipulating the linkedList can be used together`` () =
 
     let val1 = pop linkedList
 
-    Assert.That(val1, Is.EqualTo(Some 20))
+    Assert.That(val1, Is.EqualTo(20))
 
     linkedList |> push 30
     let val2 = shift linkedList
 
-    Assert.That(val2, Is.EqualTo(Some 10))
+    Assert.That(val2, Is.EqualTo(10))
 
     linkedList |> unshift 40
     linkedList |> push 50
@@ -125,6 +109,6 @@ let ``All methods of manipulating the linkedList can be used together`` () =
     let val4 = pop linkedList
     let val5 = shift linkedList
 
-    Assert.That(val3, Is.EqualTo(Some 40))
-    Assert.That(val4, Is.EqualTo(Some 50))
-    Assert.That(val5, Is.EqualTo(Some 30))
+    Assert.That(val3, Is.EqualTo(40))
+    Assert.That(val4, Is.EqualTo(50))
+    Assert.That(val5, Is.EqualTo(30))
