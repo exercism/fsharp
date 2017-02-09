8445219ef0eb1a1a3c865079801c67247f967be6
Botond Balázs <balazsbotond@gmail.com>
2017-02-09 16:37:07 +0100

    Acronym: Replace [<TestCase>] tests with individual tests (#298) (#299)
    * Fix #286 - Linked List: change pop and shift to return option
    
    * Linked List: Add missing Ignore attribute
    
    * Markdown: Use correct tags for italic and bold text
    
    * Revert "Fix #286 - Linked List: change pop and shift to return option"
    
    This reverts commit 9da0115e6543e5850d93fcbb54b7e3bf685d9d40.
    
    * Acronym: Replace [<TestCase>] tests with individual tests (#298)
    
    * acronym: Add missing [<TestCase>] attribute
    
    * Add missing [<Ignore>] attributes

--- exercises/acronym/AcronymTest.fs
+++ exercises/acronym/AcronymTest.fs
@@ -6,12 +6,59 @@ open Acronym
 [<Test>]
 let ``Empty string abbreviated to empty string`` () =
     Assert.That(acronym "", Is.EqualTo(""))
-        
-[<TestCase("Portable Network Graphics", ExpectedResult = "PNG", Ignore = "Remove to run test case")>]
-[<TestCase("Ruby on Rails", ExpectedResult = "ROR", Ignore = "Remove to run test case")>]
-[<TestCase("HyperText Markup Language", ExpectedResult = "HTML", Ignore = "Remove to run test case")>]
-[<TestCase("First In, First Out", ExpectedResult = "FIFO", Ignore = "Remove to run test case")>]
-[<TestCase("PHP: Hypertext Preprocessor", ExpectedResult = "PHP", Ignore = "Remove to run test case")>]
-[<TestCase("Complementary metal-oxide semiconductor", ExpectedResult = "CMOS", Ignore = "Remove to run test case")>]
-let ``Phrase abbreviated to acronym`` (phrase: string) =
-    acronym phrase
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Basic`` () =
+    let phrase = "Portable Network Graphics"
+    let expected = "PNG"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Lowercase words`` () =
+    let phrase = "Ruby on Rails"
+    let expected = "ROR"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Camel case`` () =
+    let phrase = "HyperText Markup Language"
+    let expected = "HTML"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Punctuation`` () =
+    let phrase = "First In, First Out"
+    let expected = "FIFO"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``All-Caps words`` () =
+    let phrase = "PHP: Hypertext Preprocessor"
+    let expected = "PHP"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Non-acronym all-caps word`` () =
+    let phrase = "GNU Image Manipulation Program"
+    let expected = "GIMP"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Hyphenated`` () =
+    let phrase = "Complementary metal-oxide semiconductor"
+    let expected = "CMOS"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
