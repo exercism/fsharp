2b04d954ef1cf314a2de00e9273840256b360626
Botond Balazs <balazsbotond@gmail.com>
2017-02-09 14:02:50 +0100

    Acronym: Replace [<TestCase>] tests with individual tests (#298)

--- exercises/acronym/AcronymTest.fs
+++ exercises/acronym/AcronymTest.fs
@@ -6,12 +6,45 @@ open Acronym
 [<Test>]
 let ``Empty string abbreviated to empty string`` () =
     Assert.That(acronym "", Is.EqualTo(""))
-        
-[<TestCase("Portable Network Graphics", ExpectedResult = "PNG", Ignore = "Remove to run test case")>]
-[<TestCase("Ruby on Rails", ExpectedResult = "ROR", Ignore = "Remove to run test case")>]
-[<TestCase("HyperText Markup Language", ExpectedResult = "HTML", Ignore = "Remove to run test case")>]
-[<TestCase("First In, First Out", ExpectedResult = "FIFO", Ignore = "Remove to run test case")>]
-[<TestCase("PHP: Hypertext Preprocessor", ExpectedResult = "PHP", Ignore = "Remove to run test case")>]
-[<TestCase("Complementary metal-oxide semiconductor", ExpectedResult = "CMOS", Ignore = "Remove to run test case")>]
-let ``Phrase abbreviated to acronym`` (phrase: string) =
-    acronym phrase
+
+let ``Basic`` () =
+    let phrase = "Portable Network Graphics"
+    let expected = "PNG"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``Lowercase words`` () =
+    let phrase = "Ruby on Rails"
+    let expected = "ROR"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``Camel case`` () =
+    let phrase = "HyperText Markup Language"
+    let expected = "HTML"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``Punctuation`` () =
+    let phrase = "First In, First Out"
+    let expected = "FIFO"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``All-Caps words`` () =
+    let phrase = "PHP: Hypertext Preprocessor"
+    let expected = "PHP"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``Non-acronym all-caps word`` () =
+    let phrase = "GNU Image Manipulation Program"
+    let expected = "GIMP"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
+
+let ``Hyphenated`` () =
+    let phrase = "Complementary metal-oxide semiconductor"
+    let expected = "CMOS"
+    let actual = acronym phrase
+    Assert.That(expected, Is.EqualTo(actual))
