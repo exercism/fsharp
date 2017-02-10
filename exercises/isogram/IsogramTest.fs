26c7f085cc652a5aedd07d76d3a9d90c7a444cdd
Botond Balázs <balazsbotond@gmail.com>
2017-02-10 21:45:36 +0100

    isogram: Replace [<TestCase>] tests with individual tests (#298) (#302)

--- exercises/isogram/IsogramTest.fs
+++ exercises/isogram/IsogramTest.fs
@@ -4,15 +4,73 @@ open NUnit.Framework
 
 open Isogram
 
-[<TestCase("duplicates", ExpectedResult = true)>]
-[<TestCase("eleven", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("subdermatoglyphic", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Alphabet", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("thumbscrew-japingly", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("the quick brown fox", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("Emily Jung Schwartzkopf", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("éléphant", ExpectedResult = false, Ignore = "Remove to run test case")>]
-let ``Isogram correctly detects isograms`` (actual: string) =
-    isogram actual
+[<Test>]
+let ``Empty string`` () =
+    let input = ""
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Isogram with only lower case characters`` () =
+    let input = "isogram"
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Word with one duplicated character`` () =
+    let input = "eleven"
+    let expected = false
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Longest reported english isogram`` () =
+    let input = "subdermatoglyphic"
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Word with duplicated character in mixed case`` () =
+    let input = "Alphabet"
+    let expected = false
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Hypothetical isogrammic word with hyphen`` () =
+    let input = "thumbscrew-japingly"
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Isogram with duplicated non letter character`` () =
+    let input = "Hjelmqvist-Gryb-Zock-Pfund-Wax"
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Made-up name that is an isogram`` () =
+    let input = "Emily Jung Schwartzkopf"
+    let expected = true
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Word with duplicated accented character`` () =
+    let input = "éléphant"
+    let expected = false
+    let actual = isogram input
+    Assert.That(actual, Is.EqualTo(expected))
