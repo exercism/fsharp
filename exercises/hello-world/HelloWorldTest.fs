41f2881eab4e264b99feb2a245e7034fac953aba
Erik Schierboom <erik_schierboom@hotmail.com>
2017-02-20 20:26:47 +0100

    Update hello-world exercise (#312)

--- exercises/hello-world/HelloWorldTest.fs
+++ exercises/hello-world/HelloWorldTest.fs
@@ -5,15 +5,5 @@ open NUnit.Framework
 open HelloWorld
 
 [<Test>]
-let ``No name`` () =
-    Assert.That(hello None, Is.EqualTo("Hello, World!"))
-
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Sample name`` () =
-    Assert.That(hello (Some "Alice"), Is.EqualTo("Hello, Alice!"))
-
-[<Test>]
-[<Ignore("Remove to run test")>]
-let ``Other sample name`` () =
-    Assert.That(hello (Some "Bob"), Is.EqualTo("Hello, Bob!"))
+let ``Say hi!`` () =
+    Assert.That(hello, Is.EqualTo("Hello, World!"))
