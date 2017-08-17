165e2550f7c46aaced2296c0ae2be3c357ee5772
Rob Keim <robkeim@gmail.com>
2017-08-18 02:53:40 +0700

    Add two-fer exercise (#340)
    * Add two-fer exercise
    
    * Fix build error
    
    * Fix build second attempt
    
    * Remove tab
    
    * Integrate PR feedback

---
+++ exercises/two-fer/TwoFerTest.fs
@@ -0,0 +1,19 @@
+﻿module TwoFerTest
+
+open NUnit.Framework
+
+open TwoFer
+
+[<Test>]
+let ``No name given`` () =
+    Assert.That(getResponse None, Is.EqualTo("One for you, one for me."))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``A name given`` () =
+    Assert.That(getResponse (Some "Alice"), Is.EqualTo("One for Alice, one for me."))
+
+[<Test>]
+[<Ignore("Remove to run test")>]
+let ``Another name given`` () =
+    Assert.That(getResponse (Some "Bob"), Is.EqualTo("One for Bob, one for me."))
