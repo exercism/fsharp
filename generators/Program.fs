e1ce4fb24a9819fd9b9ec142a4645c4bdefb9ce9
Erik Schierboom <erik_schierboom@hotmail.com>
2017-09-11 09:53:57 +0200

    Add Serilog support

--- generators/Program.fs
+++ generators/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open CommandLine
+open Serilog
 
 type Options = {
   [<CommandLine.Option('e', "exercises", Required = false, 
@@ -10,18 +11,32 @@ type Options = {
     HelpText = "Use the cached canonical data and don't update the data.")>] CacheCanonicalData : bool;
 }
 
-let run options = 
-  printfn "%A" options
-  0
+let parseOptions argv =  
+  let result = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+  match result with
+  | :? Parsed<Options> as parsed -> Result.Ok(parsed.Value)
+  | :? NotParsed<Options> as notParsed -> Result.Error(notParsed.Errors |> Seq.map string)
+  | _ -> Result.Error(seq { yield "Error parsing command-line arguments" })
 
-let fail errors =
-  printfn "Errors: %A\n" errors
-  1
+let setupLogger() =
+  Log.Logger <- LoggerConfiguration()
+      .WriteTo.LiterateConsole()
+      .CreateLogger();
+
+let regenerateTestClasses options =
+  Log.Information("Re-generating test classes...")
+
+
+  Log.Information("Re-generated test classes.")
 
 [<EntryPoint>]
-let main argv =
-    let result = CommandLine.Parser.Default.ParseArguments<Options>(argv)
-    match result with
-    | :? Parsed<Options> as parsed -> run parsed.Value
-    | :? NotParsed<Options> as notParsed -> fail notParsed.Errors
-    | _ -> fail "Error parsing arguments"
+let main argv = 
+  setupLogger()
+
+  match parseOptions argv with
+  | Result.Ok(options) -> 
+    regenerateTestClasses options
+    0
+  | Result.Error(errors) -> 
+    Log.Error("Error(s) parsing commandline arguments: {Errors}", errors)
+    1
