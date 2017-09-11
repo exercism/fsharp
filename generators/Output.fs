feb397613ee576d24d2b22d795ff6f22a15eb177
Erik Schierboom <erik_schierboom@hotmail.com>
2017-09-11 18:49:31 +0200

    Use embedded templates

--- generators/Output.fs
+++ generators/Output.fs
@@ -1,15 +1,16 @@
 module Generators.Output
 
 open System.Collections.Generic
+open System.IO
 open System.Reflection
 open FSharp.Reflection
 open DotLiquid
 open DotLiquid.FileSystems
 open Input
 
-let registrations = Dictionary<_,_>()
+let private registrations = Dictionary<_,_>()
 
-let parseTemplate<'T> template =
+let private parseTemplate<'T> template =
     let rec registerTypeTree ty =
         if registrations.ContainsKey ty then ()
         elif FSharpType.IsRecord ty then
@@ -37,7 +38,8 @@ let parseTemplate<'T> template =
     // Given a label name and an instance of the model, render the template with a dictionary made of all of the properties of the model
     fun k (v:'T) -> t.Render(Hash.FromDictionary(dict [k, box v]))
 
-Template.FileSystem <- EmbeddedFileSystem(Assembly.GetEntryAssembly(), "") :> DotLiquid.FileSystems.IFileSystem
+let private fileSystem = EmbeddedFileSystem(Assembly.GetExecutingAssembly(), "")
+Template.FileSystem <- fileSystem :> DotLiquid.FileSystems.IFileSystem
 Template.NamingConvention <- NamingConventions.CSharpNamingConvention()
 
 let renderInline<'T> template label parameters =
@@ -45,5 +47,5 @@ let renderInline<'T> template label parameters =
     template parameters
 
 let renderPartial<'T> templateName label parameters =
-    let template = parseTemplate<'T> (sprintf "{{%% include \"%s\" %%}}" templateName) label
+    let template = parseTemplate<'T> (sprintf "{%% include \"%s\" %%}" templateName) label
     template parameters
