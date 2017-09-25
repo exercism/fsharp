9305a8bec00da7416b482f0cdb1c782b0c20abb7
Erik Schierboom <erik_schierboom@hotmail.com>
2017-09-25 10:01:21 +0200

    Add beer-song generator
    Also adds support for rendering

--- generators/Exercises.fs
+++ generators/Exercises.fs
@@ -11,7 +11,7 @@ open Input
 open Output
 
 [<AbstractClass>]
-type Exercise() =    
+type Exercise() =
     abstract member ToTestClass : CanonicalData -> TestClass
     abstract member ToTestMethod : int -> CanonicalDataCase -> TestMethod
     abstract member ToTestMethodBody : CanonicalDataCase -> TestMethodBody  
@@ -19,14 +19,17 @@ type Exercise() =
     abstract member RenderTestMethod : int -> CanonicalDataCase -> string
     abstract member RenderTestMethodBody : CanonicalDataCase -> string
     abstract member RenderExpected : obj -> string
+    abstract member RenderArrange : CanonicalDataCase -> string list
     abstract member RenderSut : CanonicalDataCase -> string
     abstract member RenderSutParameters : CanonicalDataCase -> string list
     abstract member RenderAssert : CanonicalDataCase -> string
     abstract member RenderSutProperty : CanonicalDataCase -> string
+    abstract member RenderValue: string -> obj -> string
+    abstract member RenderValueWithIdentifier: string -> obj -> string
     abstract member SutParameters : CanonicalDataCase -> CanonicalDataCase
     abstract member MapCanonicalData : CanonicalData -> CanonicalData
-    abstract member FormatValue: string -> obj -> string  
-
+    abstract member PropertiesWithIdentifier : string list
+    
     member this.Name = this.GetType().Name.Kebaberize()
     member this.TestModuleName = this.GetType().Name.Pascalize() |> sprintf "%sTest"
     member this.TestedModuleName = this.GetType().Name.Pascalize()
@@ -61,12 +64,12 @@ type Exercise() =
           Body = this.RenderTestMethodBody canonicalDataCase }
 
     default this.ToTestMethodBody canonicalDataCase =         
-        { Arrange = []
+        { Arrange = this.RenderArrange canonicalDataCase
           Assert = this.RenderAssert canonicalDataCase
           Sut = this.RenderSut canonicalDataCase
           Expected = this.RenderExpected canonicalDataCase.["expected"] }
 
-    default this.RenderExpected expected = this.FormatValue "expected" expected
+    default this.RenderExpected expected = this.RenderValue "expected" expected
 
     default this.Render canonicalData =
         canonicalData
@@ -83,6 +86,11 @@ type Exercise() =
         |> this.ToTestMethodBody
         |> renderPartial "TestMethodBody"
 
+    default this.RenderArrange canonicalDataCase =
+        canonicalDataCase
+        |> Map.filter (fun key _ -> List.contains key this.PropertiesWithIdentifier)
+        |> Map.fold (fun acc key value -> this.RenderValueWithIdentifier key value :: acc) []
+
     default this.RenderSut canonicalDataCase = 
         let parameters = this.RenderSutParameters canonicalDataCase
         let property = this.RenderSutProperty canonicalDataCase
@@ -92,7 +100,8 @@ type Exercise() =
 
     default this.RenderSutParameters canonicalDataCase =
         let sutParameters = this.SutParameters canonicalDataCase
-        Map.foldBack (fun key value acc -> this.FormatValue key value :: acc) sutParameters [] 
+        
+        Map.foldBack (fun key value acc -> this.RenderValue key value :: acc) sutParameters [] 
 
     default this.RenderSutProperty canonicalDataCase = 
         string canonicalDataCase.["property"]
@@ -103,13 +112,26 @@ type Exercise() =
         |> Map.remove "expected"
         |> Map.remove "description"
 
-    default this.FormatValue key value = formatValue value    
+    default this.RenderValue key value = 
+        if (List.contains key this.PropertiesWithIdentifier) then
+            key
+        else
+            formatValue value
+
+    default this.RenderValueWithIdentifier key value = sprintf "let %s = %s" key (formatValue value)
+
+    default this.PropertiesWithIdentifier = []
 
 type Acronym() =
     inherit Exercise()
 
 type AtbashCipher() =
     inherit Exercise()
+
+type BeerSong() =
+    inherit Exercise()
+
+    override this.PropertiesWithIdentifier = ["expected"]
 
 type Bob() =
     inherit Exercise()
@@ -125,7 +147,7 @@ type Gigasecond() =
 
     let format = formatDateTime >> parenthesize
 
-    override this.FormatValue key value =
+    override this.RenderValue key value =
         match key with
         | "input" -> 
             DateTime.Parse(string value, CultureInfo.InvariantCulture) |> format
