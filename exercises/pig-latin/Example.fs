0d0ac32c76f836969dd6119e336960f218942480
Erik Schierboom <erik_schierboom@hotmail.com>
2017-10-04 17:41:34 +0200

    Fix pig-latin exercise

--- exercises/pig-latin/Example.fs
+++ exercises/pig-latin/Example.fs
@@ -3,7 +3,7 @@
 open System.Text.RegularExpressions
 
 let vowelRegex = Regex(@"(?<begin>^|\s+)(?<vowel>a|e|i|o|u|yt|xr)(?<rest>\w+)", RegexOptions.Compiled)
-let consonantRegex = Regex(@"(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|sch|yt|\wqu|\w)(?<rest>\w+)", RegexOptions.Compiled)
+let consonantRegex = Regex(@"(?<begin>^|\s+)(?<consonant>ch|qu|thr|th|rh|sch|yt|\wqu|\w)(?<rest>\w+)", RegexOptions.Compiled)
 
 let vowelReplacement = "${begin}${vowel}${rest}ay";
 let consonantReplacement = "${begin}${rest}${consonant}ay";
