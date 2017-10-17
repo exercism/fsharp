ee10356e52b15662b7371bcf1995a4670022cbbe
Erik Schierboom <erik_schierboom@hotmail.com>
2017-10-17 17:29:27 +0200

    Add grains generator (#406)

--- exercises/grains/Example.fs
+++ exercises/grains/Example.fs
@@ -2,5 +2,13 @@
 
 open System.Numerics
 
-let square (n:int) = 2I ** (n - 1)
-let total = [1..64] |> List.sumBy square
+let square (n:int) = 
+    if n <= 0 || n > 64 then 
+        Error "Invalid input"
+    else
+        Ok (2I ** (n - 1) |> uint64)
+
+let total = 
+    [1..64] 
+    |> List.map square
+    |> List.reduce (fun x y -> match x, y with Ok a, Ok b -> Ok (a + b) | _ -> Error "Invalid input")
