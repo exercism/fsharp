a03fa3a4f5d14e2a384c1c4d36cfb775c23939ca
vrnithinkumar <vrnithinkumar@gmail.com>
2017-10-24 01:44:58 +0530

    Adding test case generator for dominoes.

--- exercises/dominoes/DominoesTest.fs
+++ exercises/dominoes/DominoesTest.fs
@@ -1,66 +1,69 @@
+// This file was auto-generated based on version 2.0.0 of the canonical data.
+
 module DominoesTest
 
-open Xunit
 open FsUnit.Xunit
+open Xunit
 
 open Dominoes
 
 [<Fact>]
 let ``Empty input = empty output`` () =
-    let actual = []
-    canChain actual |> should equal true
+    let input = []
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Singleton input = singleton output`` () =
-    let actual = [(1, 1)]
-    canChain actual |> should equal true
+    let input = [(1, 1)]
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Singleton that can't be chained`` () =
-    let actual = [(1, 2)]
-    canChain actual |> should equal false
+    let input = [(1, 2)]
+    canChain input |> should equal false
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Three elements`` () =
-    let actual = [(1, 2); (3, 1); (2, 3)]
-    canChain actual |> should equal true
+    let input = [(1, 2); (3, 1); (2, 3)]
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Can reverse dominoes`` () =
-    let actual = [(1, 2); (1, 3); (2, 3)]
-    canChain actual |> should equal true
+    let input = [(1, 2); (1, 3); (2, 3)]
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Can't be chained`` () =
-    let actual = [(1, 2); (4, 1); (2, 3)]
-    canChain actual |> should equal false
+    let input = [(1, 2); (4, 1); (2, 3)]
+    canChain input |> should equal false
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Disconnected - simple`` () =
-    let actual = [(1, 1); (2, 2)]
-    canChain actual |> should equal false
+    let input = [(1, 1); (2, 2)]
+    canChain input |> should equal false
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Disconnected - double loop`` () =
-    let actual = [(1, 2); (2, 1); (3, 4); (4, 3)]
-    canChain actual |> should equal false
+    let input = [(1, 2); (2, 1); (3, 4); (4, 3)]
+    canChain input |> should equal false
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Disconnected - single isolated`` () =
-    let actual = [(1, 2); (2, 3); (3, 1); (4, 4)]
-    canChain actual |> should equal false
+    let input = [(1, 2); (2, 3); (3, 1); (4, 4)]
+    canChain input |> should equal false
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Need backtrack`` () =
-    let actual = [(1, 2); (2, 3); (3, 1); (2, 4); (2, 4)]
-    canChain actual |> should equal true
+    let input = [(1, 2); (2, 3); (3, 1); (2, 4); (2, 4)]
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Separate loops`` () =
-    let actual = [(1, 2); (2, 3); (3, 1); (1, 1); (2, 2); (3, 3)]
-    canChain actual |> should equal true
+    let input = [(1, 2); (2, 3); (3, 1); (1, 1); (2, 2); (3, 3)]
+    canChain input |> should equal true
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Ten elements`` () =
-    let actual = [(1, 2); (5, 3); (3, 1); (1, 2); (2, 4); (1, 6); (2, 3); (3, 4); (5, 6)]
-    canChain actual |> should equal true
+let ``Nine elements`` () =
+    let input = [(1, 2); (5, 3); (3, 1); (1, 2); (2, 4); (1, 6); (2, 3); (3, 4); (5, 6)]
+    canChain input |> should equal true
+
