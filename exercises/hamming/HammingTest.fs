415782e4c2921611c582ccb6a487c0b2ad6eb027
Erik Schierboom <erik_schierboom@hotmail.com>
2017-10-28 10:01:32 +0200

    Add hamming generator

--- exercises/hamming/HammingTest.fs
+++ exercises/hamming/HammingTest.fs
@@ -1,29 +1,69 @@
+// This file was auto-generated based on version 2.0.1 of the canonical data.
+
 module HammingTest
 
-open Xunit
 open FsUnit.Xunit
+open Xunit
+
 open Hamming
 
 [<Fact>]
-let ``No difference between empty strands`` () =
-    compute "" "" |> should equal 0
+let ``Empty strands`` () =
+    distance "" "" |> should equal (Some 0)
 
 [<Fact(Skip = "Remove to run test")>]
-let ``No difference between identical strands`` () =
-    compute "GGACTGA" "GGACTGA" |> should equal 0
+let ``Identical strands`` () =
+    distance "A" "A" |> should equal (Some 0)
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Complete hamming distance in small strand`` () =
-    compute "ACT" "GGA" |> should equal 3
+let ``Long identical strands`` () =
+    distance "GGACTGA" "GGACTGA" |> should equal (Some 0)
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Hamming distance is off by one strand`` () =
-    compute "GGACGGATTCTG" "AGGACGGATTCT" |> should equal 9
+let ``Complete distance in single nucleotide strands`` () =
+    distance "A" "G" |> should equal (Some 1)
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Smalling hamming distance in middle somewhere`` () =
-    compute "GGACG" "GGTCG" |> should equal 1
+let ``Complete distance in small strands`` () =
+    distance "AG" "CT" |> should equal (Some 2)
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Larger distance`` () =
-    compute "ACCAGGG" "ACTATGG" |> should equal 2
+let ``Small distance in small strands`` () =
+    distance "AT" "CT" |> should equal (Some 1)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Small distance`` () =
+    distance "GGACG" "GGTCG" |> should equal (Some 1)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Small distance in long strands`` () =
+    distance "ACCAGGG" "ACTATGG" |> should equal (Some 2)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Non-unique character in first strand`` () =
+    distance "AAG" "AAA" |> should equal (Some 1)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Non-unique character in second strand`` () =
+    distance "AAA" "AAG" |> should equal (Some 1)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Same nucleotides in different positions`` () =
+    distance "TAG" "GAT" |> should equal (Some 2)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Large distance`` () =
+    distance "GATACA" "GCATAA" |> should equal (Some 4)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Large distance in off-by-one strand`` () =
+    distance "GGACGGATTCTG" "AGGACGGATTCT" |> should equal (Some 9)
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Disallow first strand longer`` () =
+    distance "AATG" "AAA" |> should equal None
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Disallow second strand longer`` () =
+    distance "ATA" "AGTG" |> should equal None
+
