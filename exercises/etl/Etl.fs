04eb689800d74b2bc1021bc9263d35ad9f70c523
Erik Schierboom <erik_schierboom@hotmail.com>
2017-10-29 19:10:09 +0100

    Add etl generator (#428)

--- exercises/etl/Etl.fs
+++ exercises/etl/Etl.fs
@@ -1,3 +1,3 @@
-﻿module ETL
+﻿module Etl
 
-let transform (scoresWithLetters: Map<int, string list>): Map<string, int> = failwith "You need to implement this function."
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = failwith "You need to implement this function."
