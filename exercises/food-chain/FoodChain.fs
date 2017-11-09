d67b1ef4865e59dff9bb9df9e25f12031debee07
Erik Schierboom <erik_schierboom@hotmail.com>
2017-11-09 16:45:10 +0100

    Update food-chain generator (#437)

--- exercises/food-chain/FoodChain.fs
+++ exercises/food-chain/FoodChain.fs
@@ -1,5 +1,3 @@
 ﻿module FoodChain
 
-let verse number: string list = failwith "You need to implement this function."
-
-let verses start stop: string list = failwith "You need to implement this function."
+let recite start stop: string list = failwith "You need to implement this function."
