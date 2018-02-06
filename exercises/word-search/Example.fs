512e6b0d293f21c5c29d091115defc092700ffc9
Erik Schierboom <erik_schierboom@hotmail.com>
2018-02-06 16:07:35 +0100

    word-search: Add generator (#513)

--- exercises/word-search/Example.fs
+++ exercises/word-search/Example.fs
@@ -1,6 +1,6 @@
 ﻿module WordSearch
 
-let directions =
+let private directions =
     [( 1,  0);
      ( 0,  1);
      (-1,  0);
@@ -10,17 +10,18 @@ let directions =
      (-1,  1);
      (-1, -1)]
      
-let update (x, y) (dx, dy) = x + dx, y + dy
+let private update (x, y) (dx, dy) = x + dx, y + dy
 
-let find (puzzle: string list) (word: string) =
-    let width = puzzle.[0].Length
-    let height = puzzle.Length
+let search (grid: string list) (wordsToSearchFor: string list) =
+    let width = List.head grid |> Seq.length
+    let height = List.length grid
 
     let findChar (x, y) = 
-        if x > 0 && x <= width && y > 0 && y <= height then Some puzzle.[y - 1].[x - 1]
+        if x > 0 && x <= width && y > 0 && y <= height then 
+            Some grid.[y - 1].[x - 1]
         else None
         
-    let findWord start direction =
+    let findWord start direction word =
         let rec helper coord last (remainder: string) =
             if remainder.Length = 0 then Some (start, last)
             elif Some remainder.[0] = findChar coord then helper (update coord direction) coord remainder.[1..]
@@ -28,10 +29,15 @@ let find (puzzle: string list) (word: string) =
 
         helper start start word
 
-    seq { for x in 1 .. width do
-            for y in 1 .. height do
-                for dir in directions do
-                    let result = findWord (x, y) dir
-                    if Option.isSome result then
-                        yield Option.get result }
-    |> Seq.tryHead
+    let tryFindWord word =
+        seq { for x in 1 .. width do
+                for y in 1 .. height do
+                    for dir in directions do
+                        let result = findWord (x, y) dir word
+                        if Option.isSome result then
+                            yield Option.get result }
+        |> Seq.tryHead
+
+    wordsToSearchFor
+    |> List.map (fun word -> (word, tryFindWord word))
+    |> Map.ofList
