7d7bda3efe1f0c1c090663db33921a45c33bb8e5
Erik Schierboom <erik_schierboom@hotmail.com>
2018-04-04 18:01:50 +0200

    yacht: add exercise

---
+++ exercises/yacht/Yacht.fs
@@ -0,0 +1,17 @@
+﻿module Yacht
+
+type Category = 
+    | Ones
+    | Twos
+    | Threes
+    | Fours
+    | Fives
+    | Sixes
+    | FullHouse
+    | FourOfAKind
+    | LittleStraight
+    | BigStraight
+    | Choice
+    | Yacht
+
+let score category dice = failwith "You need to implement this function."
