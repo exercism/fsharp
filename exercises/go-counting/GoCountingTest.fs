817c6bc76305bc5eec3fca7507ceaec65bc70fe6
Erik Schierboom <erik_schierboom@hotmail.com>
2018-04-08 12:00:57 +0200

    go-counting: add generator (#533)

--- exercises/go-counting/GoCountingTest.fs
+++ exercises/go-counting/GoCountingTest.fs
@@ -1,78 +1,137 @@
-// This file was created manually and its version is 1.0.0.
+// This file was auto-generated based on version 1.0.0 of the canonical data.
 
 module GoCountingTest
 
-open System
-
-open Xunit
 open FsUnit.Xunit
+open Xunit
 
 open GoCounting
 
-let board5x5 =
-    ["  B  ";
-     " B B ";
-     "B W B";
-     " W W ";
-     "  W  "]
-
-let board9x9 =
-    ["  B   B  ";
-     "B   B   B";
-     "WBBBWBBBW";
-     "W W W W W";
-     "         ";
-     " W W W W ";
-     "B B   B B";
-     " W BBB W ";
-     "   B B   "]
-
 [<Fact>]
-let ``5x5 territory for black`` () =
-    let expected = Some (Some Black, [(0, 0); (0, 1); (1, 0)])
-    territoryFor board5x5 (0, 1) |> should equal expected
+let ``Black corner territory on 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (0, 1)
+    let expected = Option.Some (Owner.Black, [(0, 0); (0, 1); (1, 0)])
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``5x5 territory for white`` () =
-    let expected = Some (Some White, [(2, 3)])
-    territoryFor board5x5 (2, 3) |> should equal expected
+let ``White center territory on 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (2, 3)
+    let expected = Option.Some (Owner.White, [(2, 3)])
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``5x5 open territory`` () =
-    let expected = Some ((None: Color option), [(0, 3); (0, 4); (1, 4)])
-    territoryFor board5x5 (1, 4) |> should equal expected
+let ``Open corner territory on 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (1, 4)
+    let expected = Option.Some (Owner.None, [(0, 3); (0, 4); (1, 4)])
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``5x5 non-territory (stone)`` () =
-    territoryFor board5x5 (1, 1) |> should equal None
+let ``A stone and not a territory on 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (1, 1)
+    let expected: (Owner * (int * int) list) option = Option.Some (Owner.None, [])
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``5x5 non-territory (too low coordinate)`` () =
-    territoryFor board5x5 (-1, 1) |> should equal None
+let ``Invalid because X is too low for 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (-1, 1)
+    let expected = Option.None
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``5x5 non-territory (too high coordinate)`` () =
-    territoryFor board5x5 (1, 5) |> should equal None
+let ``Invalid because X is too high for 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (5, 1)
+    let expected = Option.None
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Minimal board, no territories`` () =
-    let input = ["B"]
-    let expected = []
-
-    territories input |> should be Empty
-
-[<Fact(Skip = "Remove to run test")>]
-let ``One territory, covering the whole board`` () =
-    let input = [" "]
-    let expected = [((None: Color option), [(0, 0)])] |> Map.ofList
-
-    territories input |> should equal expected
+let ``Invalid because Y is too low for 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (1, -1)
+    let expected = Option.None
+    territory board position |> should equal expected
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Two territories, rectangular board`` () =
-    let input = [" BW "; " BW "]
-    let expected = [(Some Black, [(0, 0); (0, 1)]);
-                    (Some White, [(3, 0); (3, 1)])]
-                   |> Map.ofList
+let ``Invalid because Y is too high for 5x5 board`` () =
+    let board = 
+        [ "  B  ";
+          " B B ";
+          "B W B";
+          " W W ";
+          "  W  " ]
+    let position = (1, 5)
+    let expected = Option.None
+    territory board position |> should equal expected
 
-    territories input |> should equal expected
+[<Fact(Skip = "Remove to run test")>]
+let ``One territory is the whole board`` () =
+    let board = [" "]
+    let expected = 
+        [ (Owner.Black, []);
+          (Owner.White, []);
+          (Owner.None, [(0, 0)]) ]
+        |> Map.ofList
+    territories board |> should equal expected
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Two territory rectangular board`` () =
+    let board = 
+        [ " BW ";
+          " BW " ]
+    let expected = 
+        [ (Owner.Black, [(0, 0); (0, 1)]);
+          (Owner.White, [(3, 0); (3, 1)]);
+          (Owner.None, []) ]
+        |> Map.ofList
+    territories board |> should equal expected
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Two region rectangular board`` () =
+    let board = [" B "]
+    let expected = 
+        [ (Owner.Black, [(0, 0); (2, 0)]);
+          (Owner.White, []);
+          (Owner.None, []) ]
+        |> Map.ofList
+    territories board |> should equal expected
+
