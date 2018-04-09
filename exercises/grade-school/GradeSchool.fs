cb7a012bae59b7c8dd292d7150caaa0e255373ff
Jay Tuley <jay@tuley.name>
2018-04-09 15:34:15 -0500

    Add Type alias to make Grade School API more clear
    The `Map<int, string list>` is really just the defacto school implementation. The tests don't care what the school's type is. If we name the type it becomes more obvious what these functions are doing. It has the added benefit of providing the implementor with a less error prone way of using a different type for the school.

--- exercises/grade-school/GradeSchool.fs
+++ exercises/grade-school/GradeSchool.fs
@@ -1,9 +1,11 @@
 ﻿module GradeSchool
 
-let empty: Map<int, string list> = failwith "You need to implement this function."
+type School = Map<int, string list>
 
-let add (student: string) (grade: int) (school: Map<int, string list>): Map<int, string list> = failwith "You need to implement this function."
+let empty: School = failwith "You need to implement this function."
 
-let roster (school: Map<int, string list>): (int * string list) list = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
 
-let grade (number: int) (school: Map<int, string list>): string list = failwith "You need to implement this function."
+let roster (school: School): (int * string list) list = failwith "You need to implement this function."
+
+let grade (number: int) (school: School): string list = failwith "You need to implement this function."
