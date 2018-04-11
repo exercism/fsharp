68dab41e20192631b4a5ecd48177dcf8599914b1
jbtule <jay+code@tuley.name>
2018-04-11 17:22:28 -0500

    Fix tests that inverted actual and expected

--- exercises/bank-account/BankAccountTest.fs
+++ exercises/bank-account/BankAccountTest.fs
@@ -1,4 +1,4 @@
-// This file was created manually and its version is 1.0.0.
+// This file was created manually and its version is 1.0.1.
 
 module BankAccountTest
 
@@ -10,7 +10,7 @@ open BankAccount
 let ``Returns empty balance after opening`` () =
     let account = mkBankAccount() |> openAccount
 
-    getBalance account |> should equal <| Some 0.0
+    getBalance account |> should equal (Some 0.0)
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Check basic balance`` () =
@@ -22,8 +22,8 @@ let ``Check basic balance`` () =
         |> updateBalance 10.0 
         |> getBalance
 
-    openingBalance |> should equal <| Some 0.0
-    updatedBalance |> should equal <| Some 10.0
+    openingBalance |> should equal (Some 0.0)
+    updatedBalance |> should equal (Some 10.0)
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Balance can increment or decrement`` () =    
@@ -40,9 +40,9 @@ let ``Balance can increment or decrement`` () =
         |> updateBalance -15.0
         |> getBalance
 
-    openingBalance |> should equal <| Some 0.0
-    addedBalance |> should equal <| Some 10.0
-    subtractedBalance |> should equal <| Some -5.0
+    openingBalance |> should equal (Some 0.0)
+    addedBalance |> should equal (Some 10.0)
+    subtractedBalance |> should equal (Some -5.0)
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Account can be closed`` () =
@@ -72,4 +72,4 @@ let ``Account can be updated from multiple threads`` () =
     |> Async.RunSynchronously
     |> ignore
 
-    getBalance account |> should equal <| Some 1000.0
+    getBalance account |> should equal (Some 1000.0)
