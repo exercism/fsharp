d88b2b58986a94e428cbdd6fb06cb9821f8a60ee
Erik Schierboom <erik_schierboom@hotmail.com>
2018-06-17 16:15:05 +0200

    generators: Use strongly typed additional namespace

--- generators/Generators.fs
+++ generators/Generators.fs
@@ -205,7 +205,7 @@ type Change() =
 type CircularBuffer() = 
     inherit GeneratorExercise()
 
-    override __.AdditionalNamespaces = [ "System" ]
+    override __.AdditionalNamespaces = [typeof<Exception>.Namespace]
 
     override __.RenderAssert _ = []
 
