5aa07fc7e343736ddc98dba8c78deca403944363
MaritimeMartin <martin.habedank@gmx.de>
2018-09-26 22:54:19 +0200

    Altered exprational and expreal signature

--- exercises/rational-numbers/RationalNumbers.fs
+++ exercises/rational-numbers/RationalNumbers.fs
@@ -12,8 +12,8 @@ let div r1 r2 = failwith "You need to implement this function."
 
 let abs r = failwith "You need to implement this function."
 
-let exprational r = failwith "You need to implement this function."
+let exprational n r = failwith "You need to implement this function."
 
-let expreal r = failwith "You need to implement this function."
+let expreal r n = failwith "You need to implement this function."
 
 let reduce r = failwith "You need to implement this function."
