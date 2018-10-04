4f19aafd4a7a7bb98b01e3b94ea4a5e2dd19cc53
Michael Priestman <michael@priestman.net>
2018-10-04 10:00:07 +0100

    Removed generic types from accumulate function.

--- exercises/accumulate/Accumulate.fs
+++ exercises/accumulate/Accumulate.fs
@@ -1,3 +1,3 @@
 ﻿module Accumulate
 
-let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = failwith "You need to implement this function."
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = failwith "You need to implement this function."
