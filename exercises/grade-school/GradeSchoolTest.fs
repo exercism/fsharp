644c3ec5579cfd2b9517ba7091ff1abf8014cf55
MaritimeMartin <martin.habedank@gmx.de>
2018-10-22 22:15:57 +0200

    Added  grade-school generator / altered example

--- exercises/grade-school/GradeSchoolTest.fs
+++ exercises/grade-school/GradeSchoolTest.fs
@@ -1,69 +1,49 @@
-// This file was created manually and its version is 1.0.0.
+// This file was auto-generated based on version 1.0.0 of the canonical data.
 
 module GradeSchoolTest
 
-open Xunit
 open FsUnit.Xunit
+open Xunit
+
 open GradeSchool
 
+let studentsToSchool (students: List<string*int>):School =
+    let schoolFolder school (name,grade) =
+        add name grade school
+    List.fold schoolFolder empty students
+
 [<Fact>]
-let ``Empty school has an empty roster`` () =
-    let school = empty
+let ``Adding a student adds them to the sorted roster`` () =
+    let school = studentsToSchool [("Aimee",2)]
+    roster school |> should equal ["Aimee"]
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Adding more student adds them to the sorted roster`` () =
+    let school = studentsToSchool [("Blair",2);("James",2);("Paul",2)]
+    roster school |> should equal ["Blair"; "James"; "Paul"]
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Adding students to different grades adds them to the same sorted roster`` () =
+    let school = studentsToSchool [("Chelsea",3);("Logan",7)]
+    roster school |> should equal ["Chelsea"; "Logan"]
+
+[<Fact(Skip = "Remove to run test")>]
+let ``Roster returns an empty list if there are no students enrolled`` () =
+    let school = studentsToSchool []
     roster school |> should be Empty
 
 [<Fact(Skip = "Remove to run test")>]
-let ``Adding a student adds them to the roster for the given grade`` () =
-    let school = empty |> add "Aimee" 2
-    let expected = ["Aimee"]
-    grade 2 school |> should equal expected
-
-[<Fact(Skip = "Remove to run test")>]
-let ``Adding more students to the same grade adds them to the roster`` () =
-    let school = 
-        empty
-        |> add "Blair" 2
-        |> add "James" 2
-        |> add "Paul" 2
-    let expected = ["Blair"; "James"; "Paul"]
-    grade 2 school |> should equal expected
-
-[<Fact(Skip = "Remove to run test")>]
-let ``Adding students to different grades adds them to the roster`` () =
-    let school = 
-        empty
-        |> add "Chelsea" 3
-        |> add "Logan" 7
-    grade 3 school |> should equal ["Chelsea"]
-    grade 7 school |> should equal ["Logan"]
+let ``Student names with grades are displayed in the same sorted roster`` () =
+    let school = studentsToSchool [("Peter",2);("Anna",1);("Barb",1);("Zoe",2);("Alex",2);("Jim",3);("Charlie",1)]
+    roster school |> should equal ["Anna"; "Barb"; "Charlie"; "Alex"; "Peter"; "Zoe"; "Jim"]
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Grade returns the students in that grade in alphabetical order`` () =
-    let school = 
-        empty
-        |> add "Franklin" 5
-        |> add "Bradley" 5
-        |> add "Jeff" 1
-    let expected = ["Bradley"; "Franklin"]
-    grade 5 school |> should equal expected
+    let school = studentsToSchool [("Franklin",5);("Bradley",5);("Jeff",1)]
+    grade 5 school |> should equal ["Bradley"; "Franklin"]
 
 [<Fact(Skip = "Remove to run test")>]
 let ``Grade returns an empty list if there are no students in that grade`` () =
-    let school = empty
+    let school = studentsToSchool []
     grade 1 school |> should be Empty
 
-[<Fact(Skip = "Remove to run test")>]
-let ``Student names and grades in roster are sorted`` () =
-    let school =
-        empty        
-        |> add "Jennifer" 4
-        |> add "Kareem" 6
-        |> add "Christopher" 4
-        |> add "Kyle" 3
-        |> add "Zoe" 4
-
-    let expected = 
-        [(3, ["Kyle"]);
-         (4, ["Christopher"; "Jennifer"; "Zoe"]);
-         (6, ["Kareem"])]
-
-    roster school |> should equal expected
