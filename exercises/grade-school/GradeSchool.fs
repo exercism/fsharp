fe2485677f3edf6ec057d746a69014b9e97a69a7
x-kej <kevin@silentplanet.com>
2018-11-01 01:35:20 -0600

    fix type mismatch in exercise:grade-school starting code (#658)

--- exercises/grade-school/GradeSchool.fs
+++ exercises/grade-school/GradeSchool.fs
@@ -6,6 +6,6 @@ let empty: School = failwith "You need to implement this function."
 
 let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
 
-let roster (school: School): (int * string list) list = failwith "You need to implement this function."
+let roster (school: School): string list = failwith "You need to implement this function."
 
 let grade (number: int) (school: School): string list = failwith "You need to implement this function."
