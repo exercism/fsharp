f46f92c3c75e6badb42f10a3d48ae7e75cb7a981
davidelettieri <davidelettieri@users.noreply.github.com>
2019-02-28 14:17:59 +0100

    improve performance of example implementation

--- exercises/pythagorean-triplet/Example.fs
+++ exercises/pythagorean-triplet/Example.fs
@@ -2,14 +2,11 @@ module PythagoreanTriplet
 
 open System
 
-let private square x = x * x
-
-let private isPythagorean (x, y, z) = square x + square y = square z
-
 let tripletsWithSum sum = 
-    [for x in 1 .. (sum - 1) do
+    [for x in 1 .. (sum / 3) do
+        let squareX = x*x
+        let z_plus_y = sum - x
         for y in (x + 1) .. (sum - 1 - x) do
-            let target = sum - (x + y)
-            let triplet = (x, y, target)
-            if isPythagorean triplet then
-                    yield triplet] |> List.distinct
+            let z = sum - (x + y)
+            if (z-y) * z_plus_y = squareX then
+                    yield (x,y,z)]
