7198278ae6609cd6fda83f82cd759456e5e78ce7
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-25 19:10:19 +0200

    [affine-cipher] Correct parameter order of stub functions

--- exercises/affine-cipher/AffineCipher.fs
+++ exercises/affine-cipher/AffineCipher.fs
@@ -1,5 +1,5 @@
 module AffineCipher
 
-let decode cipheredText a b = failwith "You need to implement this function."
+let decode a b cipheredText = failwith "You need to implement this function."
 
-let encode plainText a b = failwith "You need to implement this function."
+let encode a b plainText = failwith "You need to implement this function."
