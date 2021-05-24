f451e55076f00b008e91a748e8b1f04e3b584f2f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-05-24 21:21:39 +0200

    Revert diamond tests

--- exercises/practice/diamond/DiamondTests.fs
+++ exercises/practice/diamond/DiamondTests.fs
@@ -1,27 +1,122 @@
-module DiamondTests
-
-open FsUnit.Xunit
-open Xunit
+module DiamondTest
 
 open Diamond
+open System
+open FsUnit.Xunit
+open Xunit
+open FsCheck
+open FsCheck.Xunit
 
-[<Fact>]
-let ``Degenerate case with a single 'A' row`` () =
-    rows "A" |> should equal ["A"]
+let split (x: string) = x.Split([| '\n' |], StringSplitOptions.None)
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Degenerate case with no row containing 3 distinct groups of spaces`` () =
-    rows "B" |> should equal [" A "; "B B"; " A "]
+let trim (x:string) = x.Trim()
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Smallest non-degenerate case with odd diamond side length`` () =
-    rows "C" |> should equal ["  A  "; " B B "; "C   C"; " B B "; "  A  "]
+let leadingSpaces (x:string) = x.Substring(0, x.IndexOfAny [|'A'..'Z'|])
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Smallest non-degenerate case with even diamond side length`` () =
-    rows "D" |> should equal ["   A   "; "  B B  "; " C   C "; "D     D"; " C   C "; "  B B  "; "   A   "]
+let trailingSpaces (x:string) = x.Substring(x.LastIndexOfAny [|'A'..'Z'|] + 1)
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Largest possible diamond`` () =
-    rows "Z" |> should equal ["                         A                         "; "                        B B                        "; "                       C   C                       "; "                      D     D                      "; "                     E       E                     "; "                    F         F                    "; "                   G           G                   "; "                  H             H                  "; "                 I               I                 "; "                J                 J                "; "               K                   K               "; "              L                     L              "; "             M                       M             "; "            N                         N            "; "           O                           O           "; "          P                             P          "; "         Q                               Q         "; "        R                                 R        "; "       S                                   S       "; "      T                                     T      "; "     U                                       U     "; "    V                                         V    "; "   W                                           W   "; "  X                                             X  "; " Y                                               Y "; "Z                                                 Z"; " Y                                               Y "; "  X                                             X  "; "   W                                           W   "; "    V                                         V    "; "     U                                       U     "; "      T                                     T      "; "       S                                   S       "; "        R                                 R        "; "         Q                               Q         "; "          P                             P          "; "           O                           O           "; "            N                         N            "; "             M                       M             "; "              L                     L              "; "               K                   K               "; "                J                 J                "; "                 I               I                 "; "                  H             H                  "; "                   G           G                   "; "                    F         F                    "; "                     E       E                     "; "                      D     D                      "; "                       C   C                       "; "                        B B                        "; "                         A                         "]
+type Letters =
+    static member Chars () =
+        Arb.Default.Char()
+        |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
 
+type DiamondPropertyAttribute () =
+    inherit PropertyAttribute(Arbitrary = [| typeof<Letters> |])
+
+[<DiamondProperty>]
+let ``First row contains 'A'`` (letter:char) =
+    let actual = make letter
+    let rows = actual |> split
+    let firstRowCharacters = rows |> Seq.head |> trim
+
+    firstRowCharacters |> should equal "A"
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``All rows must have symmetric contour`` (letter:char) =
+    let actual = make letter
+    let rows = actual |> split
+    let symmetric (row:string) = leadingSpaces row = trailingSpaces row
+
+    rows |> Array.iter (fun x -> symmetric x |> should equal true)
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``Top of figure has letters in correct order`` (letter:char) =
+    let actual = make letter
+
+    let expected = ['A'..letter]
+    let rows = actual |> split
+    let firstNonSpaceLetters =
+        rows 
+        |> Seq.take expected.Length
+        |> Seq.map (trim >> Seq.head)
+        |> Seq.toList
+
+    expected |> should equal firstNonSpaceLetters
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``Figure is symmetric around the horizontal axis`` (letter:char) =
+    let actual = make letter
+
+    let rows = actual |> split
+    let top = 
+        rows
+        |> Seq.takeWhile (fun x -> not (x.Contains(string letter)))
+        |> List.ofSeq
+    
+    let bottom = 
+        rows 
+        |> Array.rev
+        |> Seq.takeWhile (fun x -> not (x.Contains(string letter)))
+        |> List.ofSeq
+
+    top |> should equal bottom
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``Diamond has square shape`` (letter:char) =
+    let actual = make letter
+
+    let rows = actual |> split
+    let expected = rows.Length
+    let correctWidth (x:string) = x.Length = expected
+
+    rows |> Array.iter (fun x -> correctWidth x |> should equal true)
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``All rows except top and bottom have two identical letters`` (letter:char) =
+    let actual = make letter
+
+    let rows = 
+        actual 
+        |> split 
+        |> Array.filter (fun x -> not (x.Contains("A")))
+
+    let twoIdenticalLetters (row:string) = 
+        let twoCharacters = row.Replace(" ", "").Length = 2
+        let identicalCharacters = row.Replace(" ", "") |> Seq.distinct |> Seq.length = 1
+        twoCharacters && identicalCharacters
+
+    rows |> Array.iter (fun x -> twoIdenticalLetters x |> should equal true)
+
+[<DiamondProperty(Skip = "Remove this Skip property to run this test")>]
+let ``Bottom left corner spaces are triangle`` (letter:char) =
+    let actual = make letter
+
+    let rows = actual |> split
+    
+    let cornerSpaces = 
+        rows 
+        |> Array.rev
+        |> Seq.skipWhile (fun x -> not (x.Contains(string letter)))
+        |> Seq.map leadingSpaces
+        |> Seq.toList
+
+    let spaceCounts = 
+        cornerSpaces 
+        |> List.map (fun x -> x.Length)
+
+    let expected = 
+        Seq.initInfinite id
+        |> Seq.take spaceCounts.Length
+        |> Seq.toList
+
+    spaceCounts |> should equal expected
