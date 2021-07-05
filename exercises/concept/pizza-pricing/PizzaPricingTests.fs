5cdfc7197c9b1f9c5e1f898c6035362e565d0257
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-05 13:06:12 +0200

    Add task to tests

--- exercises/concept/pizza-pricing/PizzaPricingTests.fs
+++ exercises/concept/pizza-pricing/PizzaPricingTests.fs
@@ -2,43 +2,55 @@ module PizzaPricingTests
 
 open FsUnit.Xunit
 open Xunit
+open Exercism.Tests
 
 open PizzaPricing
 
 [<Fact>]
+[<Task(2)>]
 let ``Price for pizza margherita``() = pizzaPrice Margherita |> should equal 7
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza formaggio``() = pizzaPrice Formaggio |> should equal 10
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza caprese``() = pizzaPrice Caprese |> should equal 9
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza margherita with extra sauce``() = pizzaPrice (ExtraSauce Margherita) |> should equal 8
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza caprese with extra toppings``() = pizzaPrice (ExtraToppings Caprese) |> should equal 11
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza formaggio with extra sauce and toppings``() =
     pizzaPrice (ExtraSauce(ExtraToppings Caprese)) |> should equal 12
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(2)>]
 let ``Price for pizza caprese with extra sauce and toppings``() =
     pizzaPrice (ExtraToppings(ExtraSauce Formaggio)) |> should equal 13
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for no pizzas``() = orderPrice [] |> should equal 0
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for single pizza caprese``() = orderPrice [ Caprese ] |> should equal 12
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for single pizza formaggio with extra sauce``() =
     orderPrice [ ExtraSauce Formaggio ] |> should equal 14
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for one pizza margherita and one pizza caprese with extra toppings``() =
     orderPrice
         [ Margherita
@@ -46,6 +58,7 @@ let ``Order price for one pizza margherita and one pizza caprese with extra topp
     |> should equal 20
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for very large order``() =
     orderPrice
         [ Margherita
@@ -58,5 +71,6 @@ let ``Order price for very large order``() =
           ExtraToppings(ExtraSauce Formaggio) ]
     |> should equal 82
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Task(3)>]
 let ``Order price for gigantic order``() = orderPrice (List.replicate 100_000 Margherita) |> should equal 700_000
