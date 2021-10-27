b8ec11625e3fa843ff4068d713e7c5aa37e20a6b
Gleb <Grenkin1988@users.noreply.github.com>
2021-10-27 15:41:52 +0200

    Update exercises/concept/tisbury-treasure-hunt/.meta/Exemplar.fs
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- exercises/concept/tisbury-treasure-hunt/.meta/Exemplar.fs
+++ exercises/concept/tisbury-treasure-hunt/.meta/Exemplar.fs
@@ -4,7 +4,7 @@ open System
 
 let getCoordinate (line: string * string): string = snd line
 
-let convertCoordinate(coordinate: string): int * char = 
+let convertCoordinate (coordinate: string): int * char = 
     Int32.Parse(string coordinate.[0]), coordinate.[1]
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
