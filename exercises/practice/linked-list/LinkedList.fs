bbfd16cd88b6d69699cea2bc3422f7beec5530f3
Aage ten Hengel <aagetenhengel@gmail.com>
2023-02-23 10:15:17 +0100

    #1153 Remove unused function 'addToEmpty' from Linked List exercise (#1155)

--- exercises/practice/linked-list/LinkedList.fs
+++ exercises/practice/linked-list/LinkedList.fs
@@ -2,8 +2,6 @@ module LinkedList
 
 let mkLinkedList () = failwith "You need to implement this function."
 
-let addToEmpty newValue linkedList = failwith "You need to implement this function."
-
 let pop linkedList = failwith "You need to implement this function."
 
 let shift linkedList = failwith "You need to implement this function."
