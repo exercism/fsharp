d394b18f8b49d22d71656d167cfcb60ed56ec32f
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-06-09 14:21:50 +0200

    Clock

--- exercises/practice/clock/Clock.fs
+++ exercises/practice/clock/Clock.fs
@@ -1,9 +1,18 @@
 module Clock
+type Clock = {
+    Hours: int
+    Minutes: int
+}
 
-let create hours minutes = failwith "You need to implement this function."
+let create hours minutes = 
+    let m = minutes % 60
+    let h = minutes / 60
+    let hours = if (hours < 0) then 24 + (hours % 24) else (hours % 24) + h
 
-let add minutes clock = failwith "You need to implement this function."
+    { Hours = hours; Minutes = m }
 
-let subtract minutes clock = failwith "You need to implement this function."
+let add minutes clock = create clock.Hours (clock.Minutes + minutes)
 
-let display clock = failwith "You need to implement this function."
+let subtract minutes clock = create clock.Hours (clock.Minutes - minutes)
+
+let display clock = sprintf "%s:%s" (clock.Hours.ToString("00")) (clock.Minutes.ToString("00"))
