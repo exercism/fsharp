4a4dddae6a93fed5dbde666917ff3314ee658c79
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-09-05 16:10:50 +0200

    Refactor Telephone number

--- exercises/practice/phone-number/PhoneNumber.fs
+++ exercises/practice/phone-number/PhoneNumber.fs
@@ -34,25 +34,27 @@ let validateLength(cleaned: string) =
     | x when x > 11 -> Error "more than 11 digits"
     | _ -> Error "incorrect number of digits"
 
-let validateCountryCode(input: string) =
-    match input.Length, input[0] with
+let validateCountryCode(input: char array) =
+    match input.Length, (input |> Array.tryHead) with
     | 10, _ -> Ok input
-    | 11, '1' -> Ok input
+    | 11, Some '1' -> Ok input
     | _ -> Error "11 digits must start with 1"
 
-let removeCountryCode(input: string) =
+let removeCountryCode(input: char array) =
     if (input.Length = 11) then input[1..] else input
 
-let validateAreaCode(input: string) =
-    match input[0] with
-    | '0' -> Error "area code cannot start with zero"
-    | '1' -> Error "area code cannot start with one"
+let validateAreaCode(input: char array) =
+    match Array.tryHead input with
+    | Some '0' -> Error "area code cannot start with zero"
+    | Some '1' -> Error "area code cannot start with one"
+    | None -> Error "Empty phone!"
     | _ -> Ok input
 
-let validateExchangeCode(input: string) =
-    match input[3] with
-    | '0' -> Error "exchange code cannot start with zero"
-    | '1' -> Error "exchange code cannot start with one"
+let validateExchangeCode(input: char array) =
+    match input |> Array.tryItem 3 with
+    | Some '0' -> Error "exchange code cannot start with zero"
+    | Some '1' -> Error "exchange code cannot start with one"
+    | None -> Error "Invalid phone!"
     | _ -> Ok input
 
 let clean input : Result<uint64, string> =
@@ -61,8 +63,9 @@ let clean input : Result<uint64, string> =
     |> validateLetters
     |> Result.bind validatePunctuation
     |> Result.bind validateLength
+    |> Result.map (fun s -> s.ToCharArray())
     |> Result.bind validateCountryCode
     |> Result.map removeCountryCode
     |> Result.bind validateAreaCode
     |> Result.bind validateExchangeCode
-    |> Result.map uint64
+    |> Result.map (fun array -> new string(array) |> uint64)
