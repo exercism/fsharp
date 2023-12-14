a000d80ccb6fd6f3d5789a5fef3efa3047222796
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-12-14 17:09:22 +0100

    RLE : almost decode

--- exercises/practice/run-length-encoding/RunLengthEncoding.fs
+++ exercises/practice/run-length-encoding/RunLengthEncoding.fs
@@ -1,20 +1,35 @@
 module RunLengthEncoding
 
+open System
+
 let rec rle input =
     match input with
-    | [] -> []
+    | [] -> "" 
     | h :: _ ->
         let sequence = input |> Seq.takeWhile (fun c -> c = h)
         let length = sequence |> Seq.length
         let entry = if length = 1 then $"{h}" else $"{length}{h}"
         let rest = input |> List.skip length
-        entry :: (rle rest)
+        entry + (rle rest)
 
-let encode(input: string) =
-    input.ToCharArray() |> Array.toList |> rle |> String.concat ""
+let encode (input:string) = 
+    input.ToCharArray() |> Array.toList |> rle
 
-let decode input =
-    failwith "You need to implement this function."
-
-encode "AABBBCCCC"
-encode "aabbbcccc"
+let rec rled input =
+    match input with
+    | [] -> ""
+    | h :: t when Char.IsDigit h ->
+        let count = h |> string |> int
+        let char = t |> Seq.head
+        new string(char, count) + (rled t[1..])
+        // let count = input |> Seq.takeWhile Char.IsDigit 
+        // let number = String.Join("", count) |> int
+        // let char = t |> Seq.skip (count |> Seq.length) |> Seq.head
+        // new string(char, number) + (rled t[1..])
+    | h :: t ->
+        (h |> string) + (rled t)
+        
+let decode (input:string) =
+    input.ToCharArray() |> Array.toList |> rled
+    
+decode "12WB12W3B24WB" 
--- exercises/practice/run-length-encoding/RunLengthEncodingTests.fs
+++ exercises/practice/run-length-encoding/RunLengthEncodingTests.fs
@@ -13,7 +13,7 @@ let ``Encode empty string`` () =
 let ``Encode single characters only are encoded without count`` () =
     encode "XYZ" |> should equal "XYZ"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Encode string with no single characters`` () =
     encode "AABBBCCCC" |> should equal "2A3B4C"
 
@@ -29,31 +29,31 @@ let ``Encode multiple whitespace mixed in string`` () =
 let ``Encode lowercase characters`` () =
     encode "aabbbcccc" |> should equal "2a3b4c"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode empty string`` () =
     decode "" |> should equal ""
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode single characters only`` () =
     decode "XYZ" |> should equal "XYZ"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode string with no single characters`` () =
     decode "2A3B4C" |> should equal "AABBBCCCC"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode single characters with repeated characters`` () =
     decode "12WB12W3B24WB" |> should equal "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode multiple whitespace mixed in string`` () =
     decode "2 hs2q q2w2 " |> should equal "  hsqq qww  "
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Decode lowercase string`` () =
     decode "2a3b4c" |> should equal "aabbbcccc"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Encode followed by decode gives original string`` () =
     "zzz ZZ  zZ" |> encode |> decode |> should equal "zzz ZZ  zZ"
 
