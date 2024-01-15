1f6b783cf8c3886791fa1ef093ff184a47b71c48
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-01-15 13:03:24 +0100

    Replace currency and locale

--- exercises/practice/ledger/Ledger.fs
+++ exercises/practice/ledger/Ledger.fs
@@ -7,6 +7,10 @@ type Locale =
     | En
     | Nl
 
+type Currency =
+    | EUR
+    | USD
+
 type Entry =
     { dat: DateTime; des: string; chg: int }
 
@@ -21,71 +25,79 @@ let parseLocale =
     | "nl-NL" -> Nl
     | _ -> failwith "Damn it"
 
+let parseCurrency =
+    function
+    | "EUR" -> EUR
+    | "USD" -> USD
+    | _ -> failwith "Damn it"
+
 let title =
     function
     | En -> "Date       | Description               | Change       "
     | Nl -> "Datum      | Omschrijving              | Verandering  "
 
-let processLine locale currency x =
-        let mutable res = "\n"
+let processLine locale (currency: Currency) line =
+    let mutable res = "\n"
 
-        if locale = "nl-NL" then
-            res <- res + x.dat.ToString("dd-MM-yyyy")
+    match locale with
+    | Nl -> res <- res + line.dat.ToString("dd-MM-yyyy")
+    | En -> res <- res + line.dat.ToString("MM\/dd\/yyyy")
 
-        if locale = "en-US" then
-            res <- res + x.dat.ToString("MM\/dd\/yyyy")
+    res <- res + " | "
 
-        res <- res + " | "
+    if line.des.Length <= 25 then
+        res <- res + line.des.PadRight(25)
+    elif line.des.Length = 25 then
+        res <- res + line.des
+    else
+        res <- res + line.des[0..21] + "..."
 
-        if x.des.Length <= 25 then res <- res + x.des.PadRight(25)
-        elif x.des.Length = 25 then res <- res + x.des
-        else res <- res + x.des[0..21] + "..."
+    res <- res + " | "
+    let c = float line.chg / 100.0
 
-        res <- res + " | "
-        let c = float x.chg / 100.0
+    if c < 0.0 then
+        match locale with
+        | Nl ->
+            match currency with
+            | USD -> res <- res + ("$ " + c.ToString("#,#0.00", CultureInfo("nl-NL"))).PadLeft(13)
 
-        if c < 0.0 then
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", CultureInfo("nl-NL"))).PadLeft(13)
+            | EUR -> res <- res + ("€ " + c.ToString("#,#0.00", CultureInfo("nl-NL"))).PadLeft(13)
 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", CultureInfo("nl-NL"))).PadLeft(13)
+        | En ->
+            match currency with
+            | USD ->
+                res <-
+                    res
+                    + ("($" + c.ToString("#,#0.00", CultureInfo("en-US")).Substring(1) + ")")
+                        .PadLeft(13)
 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <-
-                        res
-                        + ("($" + c.ToString("#,#0.00", CultureInfo("en-US")).Substring(1) + ")")
-                            .PadLeft(13)
+            | EUR ->
+                res <-
+                    res
+                    + ("(€" + c.ToString("#,#0.00", CultureInfo("en-US")).Substring(1) + ")")
+                        .PadLeft(13)
+    else
+        match locale with
+        | Nl ->
+            match currency with
+            | USD -> res <- res + ("$ " + c.ToString("#,#0.00", CultureInfo("nl-NL")) + " ").PadLeft(13)
 
-                if currency = "EUR" then
-                    res <-
-                        res
-                        + ("(€" + c.ToString("#,#0.00", CultureInfo("en-US")).Substring(1) + ")")
-                            .PadLeft(13)
-        else
-            if locale = "nl-NL" then
-                if currency = "USD" then
-                    res <- res + ("$ " + c.ToString("#,#0.00", CultureInfo("nl-NL")) + " ").PadLeft(13)
+            | EUR -> res <- res + ("€ " + c.ToString("#,#0.00", CultureInfo("nl-NL")) + " ").PadLeft(13)
 
-                if currency = "EUR" then
-                    res <- res + ("€ " + c.ToString("#,#0.00", CultureInfo("nl-NL")) + " ").PadLeft(13)
+        | En ->
+            match currency with
+            | USD -> res <- res + ("$" + c.ToString("#,#0.00", CultureInfo("en-US")) + " ").PadLeft(13)
 
-            if locale = "en-US" then
-                if currency = "USD" then
-                    res <- res + ("$" + c.ToString("#,#0.00", CultureInfo("en-US")) + " ").PadLeft(13)
+            | EUR -> res <- res + ("€" + c.ToString("#,#0.00", CultureInfo("en-US")) + " ").PadLeft(13)
 
-                if currency = "EUR" then
-                    res <- res + ("€" + c.ToString("#,#0.00", CultureInfo("en-US")) + " ").PadLeft(13)
-                    
-        res
-        
+    res
+
 let formatLedger currency locale entries =
-    let l = locale |> parseLocale
-    let mutable res = title l
+    let locale = locale |> parseLocale
+    let currency = currency |> parseCurrency
+    let mutable res = title locale
 
     for x in List.sortBy (fun x -> x.dat, x.des, x.chg) entries do
-        res <- res + (x|> processLine locale currency)
+        res <- res + (x |> processLine locale currency)
 
     res
