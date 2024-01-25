aa3ebc04ac6a04325fd4ac8398e7d039da6bcf6c
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-25 12:52:06 +0100

    Stub file changed

--- exercises/practice/leap/Leap.fs
+++ exercises/practice/leap/Leap.fs
@@ -1,3 +1,4 @@
 module Leap
 
-let leapYear (year: int): bool = failwith "You need to implement this function."
+let leapYear (year: int): bool = 
+    failwith "You need to implement this function."
