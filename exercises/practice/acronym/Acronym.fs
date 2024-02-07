0594ee4c39148ff4728dc7726dfc38227736f074
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-07 09:52:46 +0100

    Solve acronym

--- exercises/practice/acronym/Acronym.fs
+++ exercises/practice/acronym/Acronym.fs
@@ -1,3 +1,11 @@
 module Acronym
 
-let abbreviate phrase = failwith "You need to implement this function."
+open System
+
+let abbreviate(phrase: string) =
+    phrase
+        .Replace('-', ' ')
+        .Replace('_', ' ')
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun s -> s[0] |> Char.ToUpper |> string)
+    |> String.concat ""
--- exercises/practice/acronym/AcronymTests.fs
+++ exercises/practice/acronym/AcronymTests.fs
@@ -9,35 +9,34 @@ open Acronym
 let ``Basic`` () =
     abbreviate "Portable Network Graphics" |> should equal "PNG"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Lowercase words`` () =
     abbreviate "Ruby on Rails" |> should equal "ROR"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Punctuation`` () =
     abbreviate "First In, First Out" |> should equal "FIFO"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``All caps word`` () =
     abbreviate "GNU Image Manipulation Program" |> should equal "GIMP"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Punctuation without whitespace`` () =
     abbreviate "Complementary metal-oxide semiconductor" |> should equal "CMOS"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Very long abbreviation`` () =
     abbreviate "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me" |> should equal "ROTFLSHTMDCOALM"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Consecutive delimiters`` () =
     abbreviate "Something - I made up from thin air" |> should equal "SIMUFTA"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Apostrophes`` () =
     abbreviate "Halley's Comet" |> should equal "HC"
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Underscore emphasis`` () =
     abbreviate "The Road _Not_ Taken" |> should equal "TRNT"
-
