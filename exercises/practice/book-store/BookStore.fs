5f641521a647c07b845da29a3772c0ff64a9be1d
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-20 14:06:00 +0100

    Start bookstore

--- exercises/practice/book-store/BookStore.fs
+++ exercises/practice/book-store/BookStore.fs
@@ -1,3 +1,21 @@
 module BookStore
 
-let total books = failwith "You need to implement this function."
+let getDiscount =
+    function
+    | 2 -> 0.95m
+    | 3 -> 0.90m
+    | 4 -> 0.8m
+    | 5 -> 0.75m
+    | _ -> 1m
+
+let computeForFiveOrLess books =
+    let distinct = List.distinct books |> List.length
+    (books |> List.length |> decimal) * 8m * getDiscount distinct
+
+let computePrice books =
+    0m
+
+let total books =
+    match List.length books with
+    | n when n <= 5 -> computeForFiveOrLess books
+    | _ -> computePrice books
--- exercises/practice/book-store/BookStoreTests.fs
+++ exercises/practice/book-store/BookStoreTests.fs
@@ -9,31 +9,31 @@ open BookStore
 let ``Only a single book`` () =
     total [1] |> should equal 8.00m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Two of the same book`` () =
     total [2; 2] |> should equal 16.00m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Empty basket`` () =
     total [] |> should equal 0.00m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Two different books`` () =
     total [1; 2] |> should equal 15.20m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Three different books`` () =
     total [1; 2; 3] |> should equal 21.60m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Four different books`` () =
     total [1; 2; 3; 4] |> should equal 25.60m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Five different books`` () =
     total [1; 2; 3; 4; 5] |> should equal 30.00m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Two groups of four is cheaper than group of five plus group of three`` () =
     total [1; 1; 2; 2; 3; 3; 4; 5] |> should equal 51.20m
 
@@ -49,7 +49,7 @@ let ``Group of four plus group of two is cheaper than two groups of three`` () =
 let ``Two each of first four books and one copy each of rest`` () =
     total [1; 1; 2; 2; 3; 3; 4; 4; 5] |> should equal 55.60m
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Two copies of each book`` () =
     total [1; 1; 2; 2; 3; 3; 4; 4; 5; 5] |> should equal 60.00m
 
@@ -76,4 +76,3 @@ let ``One group of one and four is cheaper than one group of two and three`` () 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``One group of one and two plus three groups of four is cheaper than one group of each size`` () =
     total [1; 2; 2; 3; 3; 3; 4; 4; 4; 4; 5; 5; 5; 5; 5] |> should equal 100.00m
-
