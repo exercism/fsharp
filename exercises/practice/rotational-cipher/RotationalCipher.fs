c90d35e403b4c190d3d5f242df3f753cc8ea1fad
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-21 09:16:20 +0100

    Solve RotationalCipher

--- exercises/practice/rotational-cipher/RotationalCipher.fs
+++ exercises/practice/rotational-cipher/RotationalCipher.fs
@@ -1,3 +1,26 @@
 module RotationalCipher
 
-let rotate shiftKey text = failwith "You need to implement this function."
+open System
+
+let (|Upper|Lower|Other|) =
+    function
+    | c when c |> Char.IsUpper -> Upper
+    | c when c |> Char.IsLower -> Lower
+    | _ -> Other
+
+let encrypt shiftKey =
+    let uppers = [ 'A' .. 'Z' ]
+    let lowers = [ 'a' .. 'z' ]
+
+    fun char ->
+        let rotate chars =
+            let idx = chars |> List.findIndex ((=) char)
+            chars[(idx + shiftKey) % 26]
+
+        match char with
+        | Upper -> uppers |> rotate
+        | Lower -> lowers |> rotate
+        | Other -> char
+
+let rotate (shiftKey: int) (text: string) =
+    text |> Seq.map (encrypt shiftKey) |> String.Concat
