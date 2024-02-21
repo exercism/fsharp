a7214bc2f495960e1b2c5d06aa0e1ae6b94b4d4c
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-02-21 11:30:51 +0100

    Meetup : ugly solve

--- exercises/practice/meetup/Meetup.fs
+++ exercises/practice/meetup/Meetup.fs
@@ -2,6 +2,64 @@ module Meetup
 
 open System
 
-type Week = First | Second | Third | Fourth | Last | Teenth
+type Week =
+    | First
+    | Second
+    | Third
+    | Fourth
+    | Last
+    | Teenth
 
-let meetup year month week dayOfWeek: DateTime = failwith "You need to implement this function."
+let isTeenth =
+    function
+    | 13
+    | 14
+    | 15
+    | 16
+    | 17
+    | 18
+    | 19 -> true
+    | _ -> false
+
+let countOk week count =
+    match week with
+    | First -> count = 1
+    | Second -> count = 2
+    | Third -> count = 3
+    | Fourth -> count = 4
+    | _ -> false
+
+let rec findDate (date: DateTime) week dayOfWeek count =
+    if date.DayOfWeek = dayOfWeek then
+        match week with
+        | Teenth ->
+            if (date.Day |> isTeenth) then
+                date
+            else
+                (findDate (date.AddDays(1)) week dayOfWeek count)
+        | First
+        | Second
+        | Third
+        | Fourth ->
+            if countOk week count then
+                date
+            else
+                (findDate (date.AddDays(1)) week dayOfWeek (count + 1))
+
+        | Last -> date
+    else
+        findDate (date.AddDays(1)) week dayOfWeek count
+
+let rec findDateRev (date: DateTime) dayOfWeek =
+    if (date.DayOfWeek = dayOfWeek) then
+        date
+    else
+        findDateRev (date.AddDays(-1)) dayOfWeek
+
+let meetup year month week dayOfWeek : DateTime =
+    let date = DateTime(year, month, 1)
+
+    if (week = Last) then
+        findDateRev (date.AddMonths(1).AddDays(-1)) dayOfWeek
+    else
+        findDate date week dayOfWeek 1
