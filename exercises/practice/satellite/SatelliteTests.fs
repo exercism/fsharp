ae22ee0756c5ce38463b8c7d1770ccfd93b41c13
Erik Schierboom <erik_schierboom@hotmail.com>
2024-10-22 15:16:26 +0200

    satellite: fix missing skips

--- exercises/practice/satellite/SatelliteTests.fs
+++ exercises/practice/satellite/SatelliteTests.fs
@@ -12,14 +12,14 @@ let ``Empty tree`` () =
     )
     treeFromTraversals [] [] |> should equal expected
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Tree with one item`` () =
     let expected: Result<Tree,string> = Ok (
         Node("a", Empty, Empty)
     )
     treeFromTraversals ["a"] ["a"] |> should equal expected
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Tree with many items`` () =
     let expected: Result<Tree,string> = Ok (
         Node(
@@ -34,17 +34,17 @@ let ``Tree with many items`` () =
     )
     treeFromTraversals ["i"; "a"; "f"; "x"; "r"] ["a"; "i"; "x"; "f"; "r"] |> should equal expected
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reject traversals of different length`` () =
     let expected: Result<Tree,string> = Error "traversals must have the same length"
     treeFromTraversals ["b"; "a"; "r"] ["a"; "b"] |> should equal expected
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reject inconsistent traversals of same length`` () =
     let expected: Result<Tree,string> = Error "traversals must have the same elements"
     treeFromTraversals ["a"; "b"; "c"] ["x"; "y"; "z"] |> should equal expected
 
-[<Fact>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reject traversals with repeated items`` () =
     let expected: Result<Tree,string> = Error "traversals must contain unique items"
     treeFromTraversals ["b"; "a"; "a"] ["a"; "b"; "a"] |> should equal expected
