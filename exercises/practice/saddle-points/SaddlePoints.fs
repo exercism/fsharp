4dfd6c43ab5cda729501f28164f3b4df6f7cf186
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-23 10:53:21 +0100

    Remove

--- exercises/practice/saddle-points/SaddlePoints.fs
+++
@@ -1,3 +0,0 @@
-module SaddlePoints
-
-let saddlePoints matrix = failwith "You need to implement this function."
--- exercises/practice/scale-generator/ScaleGenerator.fs
+++
@@ -1,5 +0,0 @@
-module ScaleGenerator
-
-let chromatic tonic = failwith "You need to implement this function."
-
-let interval intervals tonic = failwith "You need to implement this function."
