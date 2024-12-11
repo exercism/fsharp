c00e66fa0eae5faa52daf95b1bc57e20d5c3115f
Erik Schierboom <erik_schierboom@hotmail.com>
2024-12-11 14:34:10 +0100

    Remove impl

--- exercises/practice/diffie-hellman/DiffieHellman.fs
+++
@@ -1,7 +0,0 @@
-module DiffieHellman
-
-let privateKey primeP = failwith "You need to implement this function."
-
-let publicKey primeP primeG privateKey = failwith "You need to implement this function."
-
-let secret primeP publicKey privateKey = failwith "You need to implement this function."
