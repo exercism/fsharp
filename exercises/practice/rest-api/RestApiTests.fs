3d50c6e1960ea0965a5451a95f08c06025e89f59
Raman But-Husaim <r.but-husaim@posteo.net>
2025-03-01 20:34:40 +0100

    rest-api: replace triple-quote strings with single-quote ones to mitigate the issue with the test runner

--- exercises/practice/rest-api/RestApiTests.fs
+++ exercises/practice/rest-api/RestApiTests.fs
@@ -2,86 +2,84 @@ module RestApiTests
 
 open FsUnit.Xunit
 open Xunit
-
 open RestApi
 
 [<Fact>]
 let ``No users`` () =
-    let database = """{"users":[]}"""
+    let database = "{\"users\":[]}"
     let url = "/users"
-    let expected = """{"users":[]}"""
+    let expected = "{\"users\":[]}"
     let api = RestApi(database)
     api.Get url |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Add user`` () =
-    let database = """{"users":[]}"""
-    let payload = """{"user":"Adam"}"""
+    let database = "{\"users\":[]}"
+    let payload = "{\"user\":\"Adam\"}"
     let url = "/add"
-    let expected = """{"name":"Adam","owes":{},"owed_by":{},"balance":0.0}"""
+    let expected = "{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Get single user`` () =
-    let database = """{"users":[{"name":"Adam","owes":{},"owed_by":{},"balance":0.0},{"name":"Bob","owes":{},"owed_by":{},"balance":0.0}]}"""
-    let payload = """{"users":["Bob"]}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{},\"balance\":0.0}]}"
+    let payload = "{\"users\":[\"Bob\"]}"
     let url = "/users"
-    let expected = """{"users":[{"name":"Bob","owes":{},"owed_by":{},"balance":0.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{},\"balance\":0.0}]}"
     let api = RestApi(database)
-    api.Get (url, payload) |> should equal expected
+    api.Get(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Both users have 0 balance`` () =
-    let database = """{"users":[{"name":"Adam","owes":{},"owed_by":{},"balance":0.0},{"name":"Bob","owes":{},"owed_by":{},"balance":0.0}]}"""
-    let payload = """{"lender":"Adam","borrower":"Bob","amount":3.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{},\"balance\":0.0}]}"
+    let payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":3.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{},"owed_by":{"Bob":3.0},"balance":3.0},{"name":"Bob","owes":{"Adam":3.0},"owed_by":{},"balance":-3.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{\"Bob\":3.0},\"balance\":3.0},{\"name\":\"Bob\",\"owes\":{\"Adam\":3.0},\"owed_by\":{},\"balance\":-3.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Borrower has negative balance`` () =
-    let database = """{"users":[{"name":"Adam","owes":{},"owed_by":{},"balance":0.0},{"name":"Bob","owes":{"Chuck":3.0},"owed_by":{},"balance":-3.0},{"name":"Chuck","owes":{},"owed_by":{"Bob":3.0},"balance":3.0}]}"""
-    let payload = """{"lender":"Adam","borrower":"Bob","amount":3.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0},{\"name\":\"Bob\",\"owes\":{\"Chuck\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Chuck\",\"owes\":{},\"owed_by\":{\"Bob\":3.0},\"balance\":3.0}]}"
+    let payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":3.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{},"owed_by":{"Bob":3.0},"balance":3.0},{"name":"Bob","owes":{"Adam":3.0,"Chuck":3.0},"owed_by":{},"balance":-6.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{\"Bob\":3.0},\"balance\":3.0},{\"name\":\"Bob\",\"owes\":{\"Adam\":3.0,\"Chuck\":3.0},\"owed_by\":{},\"balance\":-6.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Lender has negative balance`` () =
-    let database = """{"users":[{"name":"Adam","owes":{},"owed_by":{},"balance":0.0},{"name":"Bob","owes":{"Chuck":3.0},"owed_by":{},"balance":-3.0},{"name":"Chuck","owes":{},"owed_by":{"Bob":3.0},"balance":3.0}]}"""
-    let payload = """{"lender":"Bob","borrower":"Adam","amount":3.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0},{\"name\":\"Bob\",\"owes\":{\"Chuck\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Chuck\",\"owes\":{},\"owed_by\":{\"Bob\":3.0},\"balance\":3.0}]}"
+    let payload = "{\"lender\":\"Bob\",\"borrower\":\"Adam\",\"amount\":3.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{"Bob":3.0},"owed_by":{},"balance":-3.0},{"name":"Bob","owes":{"Chuck":3.0},"owed_by":{"Adam":3.0},"balance":0.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{\"Bob\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Bob\",\"owes\":{\"Chuck\":3.0},\"owed_by\":{\"Adam\":3.0},\"balance\":0.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Lender owes borrower`` () =
-    let database = """{"users":[{"name":"Adam","owes":{"Bob":3.0},"owed_by":{},"balance":-3.0},{"name":"Bob","owes":{},"owed_by":{"Adam":3.0},"balance":3.0}]}"""
-    let payload = """{"lender":"Adam","borrower":"Bob","amount":2.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{\"Bob\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{\"Adam\":3.0},\"balance\":3.0}]}"
+    let payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":2.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{"Bob":1.0},"owed_by":{},"balance":-1.0},{"name":"Bob","owes":{},"owed_by":{"Adam":1.0},"balance":1.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{\"Bob\":1.0},\"owed_by\":{},\"balance\":-1.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{\"Adam\":1.0},\"balance\":1.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Lender owes borrower less than new loan`` () =
-    let database = """{"users":[{"name":"Adam","owes":{"Bob":3.0},"owed_by":{},"balance":-3.0},{"name":"Bob","owes":{},"owed_by":{"Adam":3.0},"balance":3.0}]}"""
-    let payload = """{"lender":"Adam","borrower":"Bob","amount":4.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{\"Bob\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{\"Adam\":3.0},\"balance\":3.0}]}"
+    let payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":4.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{},"owed_by":{"Bob":1.0},"balance":1.0},{"name":"Bob","owes":{"Adam":1.0},"owed_by":{},"balance":-1.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{\"Bob\":1.0},\"balance\":1.0},{\"name\":\"Bob\",\"owes\":{\"Adam\":1.0},\"owed_by\":{},\"balance\":-1.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
+    api.Post(url, payload) |> should equal expected
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Lender owes borrower same as new loan`` () =
-    let database = """{"users":[{"name":"Adam","owes":{"Bob":3.0},"owed_by":{},"balance":-3.0},{"name":"Bob","owes":{},"owed_by":{"Adam":3.0},"balance":3.0}]}"""
-    let payload = """{"lender":"Adam","borrower":"Bob","amount":3.0}"""
+    let database = "{\"users\":[{\"name\":\"Adam\",\"owes\":{\"Bob\":3.0},\"owed_by\":{},\"balance\":-3.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{\"Adam\":3.0},\"balance\":3.0}]}"
+    let payload = "{\"lender\":\"Adam\",\"borrower\":\"Bob\",\"amount\":3.0}"
     let url = "/iou"
-    let expected = """{"users":[{"name":"Adam","owes":{},"owed_by":{},"balance":0.0},{"name":"Bob","owes":{},"owed_by":{},"balance":0.0}]}"""
+    let expected = "{\"users\":[{\"name\":\"Adam\",\"owes\":{},\"owed_by\":{},\"balance\":0.0},{\"name\":\"Bob\",\"owes\":{},\"owed_by\":{},\"balance\":0.0}]}"
     let api = RestApi(database)
-    api.Post (url, payload) |> should equal expected
-
+    api.Post(url, payload) |> should equal expected
